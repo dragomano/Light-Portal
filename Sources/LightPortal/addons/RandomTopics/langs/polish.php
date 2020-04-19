e2bce144478c827bb251989defdd7dce241a6a12
Adrek <spam@smf.net.pl>
2020-04-19 20:42:13 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/RandomTopics/langs/polish.php
@@ -0,0 +1,6 @@
+<?php
+
+$txt['lp_block_types']['random_topics']              = 'Losowe wątki';
+$txt['lp_block_types_descriptions']['random_topics'] = 'Wyświetla listę losowych wątków z forum';
+
+$txt['lp_random_topics_addon_num_topics'] = 'Liczba wyświetlanych wątków';
