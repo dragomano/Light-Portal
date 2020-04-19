fcabe551bfacf01a97f197830d929d8cd9fd6492
Adrek <spam@smf.net.pl>
2020-04-19 20:38:17 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/PageList/langs/polish.php
@@ -0,0 +1,18 @@
+<?php
+
+$txt['lp_block_types']['page_list']              = 'Lista stron';
+$txt['lp_block_types_descriptions']['page_list'] = 'Wyświetla listę aktywnych stron portalu.';
+
+$txt['lp_page_list_addon_sort']     = 'Sortowanie stron';
+$txt['lp_page_list_addon_sort_set'] = array(
+    'page_id'     => 'Według ID',
+    'author_name' => 'Według autora',
+    'title'       => 'Według tytułu',
+    'alias'       => 'Według aliasu',
+    'type'        => 'Według typu',
+    'num_views'   => 'Według liczby wyświetleń',
+    'created_at'  => 'Według daty utworzenia',
+    'updated_at'  => 'Według daty aktualizacji'
+);
+
+$txt['lp_page_list_addon_no_items'] = 'Brak dostępnych stron.';
