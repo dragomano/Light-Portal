dd3b4d578fbbdbb27f12de05ab24d64b00354ff7
Adrek <spam@smf.net.pl>
2020-04-19 20:25:14 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/KarmaPostRating/langs/polish.php
@@ -0,0 +1,3 @@
+<?php
+
+$txt['lp_karma_post_rating_description'] = 'Integracja z modyfikacją <a href="https://dragomano.ru/mods/karma-post-rating" target="_blank" rel="noopener">Karma Post Rating</a>, wyświetla oceny wątków (jeśli dostęne) w trybie artykułów na stronie głównej.';
