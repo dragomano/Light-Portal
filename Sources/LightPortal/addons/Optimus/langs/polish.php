94e0ac2ce458dcb9a845f11949be682666ff5877
Adrek <spam@smf.net.pl>
2020-04-19 20:35:26 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/Optimus/langs/polish.php
@@ -0,0 +1,3 @@
+<?php
+
+$txt['lp_optimus_description'] = 'Wyświetla opis wątku (jeśli dostępne) jako ops artykułu.<br>Działą po włączeniu trybu artykułów: Strona główna portalu — Wszystkie wątki z wybranych działów.';
