588a2db3126712fc3b367e2d721b43d18782c261
Adrek <spam@smf.net.pl>
2020-04-19 20:22:07 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/EasyMarkdownEditor/langs/polish.php
@@ -0,0 +1,11 @@
+<?php
+
+$txt['lp_block_types']['md']                = 'Własny Markdown';
+$txt['lp_block_types_descriptions']['md']   = 'W tym bloku możesz użyć w zawartości podstawowej składni Markdown.';
+$txt['lp_easy_markdown_editor_description'] = 'Dodaje blok z własną zawartością Markdown oraz edytorem.';
+
+$txt['lp_easy_markdown_editor_addon_tasks']  = 'Lista zadań';
+$txt['lp_easy_markdown_editor_addon_lines']  = 'linii: ';
+$txt['lp_easy_markdown_editor_addon_words']  = 'słów: ';
+$txt['lp_easy_markdown_editor_addon_toggle'] = 'Przełącz obok siebie';
+$txt['lp_easy_markdown_editor_addon_guide']  = 'Poradnik Markdown';
