3cc16b8c29f84099ed526f0d1ed1c87a8b931610
Adrek <spam@smf.net.pl>
2020-04-19 21:04:14 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/TopPosters/langs/polish.php
@@ -0,0 +1,8 @@
+<?php
+
+$txt['lp_block_types']['top_posters']              = 'Najaktywniejsi';
+$txt['lp_block_types_descriptions']['top_posters'] = 'Wyświetla listę najaktywniejszych użytkowników.';
+
+$txt['lp_top_posters_addon_show_avatars']      = 'Wyświetlaj awatary użytkowników';
+$txt['lp_top_posters_addon_num_posters']       = 'Liczba wyświetlanych użytkowników';
+$txt['lp_top_posters_addon_show_numbers_only'] = 'Wyświetlaj tylko liczbę';
