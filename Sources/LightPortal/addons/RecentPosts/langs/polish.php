4d4ea6f7c044cf55f62495247e70fba8cb84e830
Adrek <spam@smf.net.pl>
2020-04-19 20:48:06 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/RecentPosts/langs/polish.php
@@ -0,0 +1,10 @@
+<?php
+
+$txt['lp_block_types']['recent_posts']              = 'Ostatnie wiadomości';
+$txt['lp_block_types_descriptions']['recent_posts'] = 'Wyświetla listę najnowszych wiadomości na forum.';
+
+$txt['lp_recent_posts_addon_num_posts']       = 'Liczba wyświetlanych wiadomości';
+$txt['lp_recent_posts_addon_type']            = 'Wyświetl link do wiadomości jako';
+$txt['lp_recent_posts_addon_type_set']        = array('link' => 'tytuł wiadomości', 'preview' => 'podgląd wiadomości');
+$txt['lp_recent_posts_addon_show_avatars']    = 'Pokaż awatary użytkowników';
+$txt['lp_recent_posts_addon_update_interval'] = 'Interwał odświeżania listy w sekundach';
