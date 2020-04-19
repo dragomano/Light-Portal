8105ad42e46e765ed5aa330daff15bfdab598bbf
Adrek <spam@smf.net.pl>
2020-04-19 20:16:10 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/BoardList/langs/polish.php
@@ -0,0 +1,7 @@
+<?php
+
+$txt['lp_block_types']['board_list']              = 'Lista działów';
+$txt['lp_block_types_descriptions']['board_list'] =  'Wyświetla listę działów dostępnych dla zalogowanego użytkownika.';
+
+$txt['lp_board_list_addon_category_class'] = 'Klasa CSS tytułów kategorii';
+$txt['lp_board_list_addon_board_class']    = 'Klasa CSS bloków listy działów';
