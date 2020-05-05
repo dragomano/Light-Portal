eb28efc8b3f766267faf89dc32e94b85e3f8ff31
Bugo <bugo@dragomano.ru>
2020-05-05 21:54:05 +0500

    Add FaBoardIcons addon

---
+++ Sources/LightPortal/addons/FaBoardIcons/langs/russian.php
@@ -0,0 +1,3 @@
+<?php
+
+$txt['lp_fa_board_icons_description'] = 'Обеспечивает интеграцию с модом <a class="bbc_link" href="https://dragomano.ru/reviews/fa-board-icons" target="_blank" rel="noopener">FA Board Icons</a>, отображая иконки разделов на главной странице портала.<br>Работает, если выбран режим «Первая страница портала — Выбранные разделы».';
---
+++ Sources/LightPortal/addons/FaBoardIcons/langs/ukrainian.php
@@ -0,0 +1,3 @@
+<?php
+
+$txt['lp_fa_board_icons_description'] = 'Забезпечує інтеграцію з модом <a class="bbc_link" href="https://dragomano.ru/reviews/fa-board-icons" target="_blank" rel="noopener">FA Board Icons</a>, відображаючи іконки розділів на головній сторінці порталу.<br>Працює, якщо вибрано режим "Перша сторінка порталу - Вибрані розділи';
