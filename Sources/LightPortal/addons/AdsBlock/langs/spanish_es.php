e4015a1cb68adb498a3f231df9ff69f55922d34a
Bomber Code <contacto@bombercode.net>
2020-05-08 15:27:14 -0300

    Update spanish_es.php

--- Sources/LightPortal/addons/AdsBlock/langs/spanish_es.php
+++ Sources/LightPortal/addons/AdsBlock/langs/spanish_es.php
@@ -1,5 +1,5 @@
 <?php
-// .spanish latin language file Rev. 1.1
+// .spanish es language file Rev. 1.1
 // Spanish translation by Rock Lee https://www.bombercode.net
 
 $txt['lp_block_types']['ads_block'] = 'Bloque de anuncios';
