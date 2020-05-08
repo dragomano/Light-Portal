5f23fd05e2023d6eaab083c29e5807fb9e073b22
Bomber Code <contacto@bombercode.net>
2020-05-08 16:03:42 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/Optimus/langs/spanish_es.php
@@ -0,0 +1,5 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_optimus_description'] = 'Muestra la descripción del tema (si está disponible) como la descripción del artículo.<br>Funciona si se selecciona el modo de tema de artículo ("La portada del portal: todos los temas de los paneles seleccionados").';
