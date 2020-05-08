419ac68c77b11cc46825860ae8a061cce1c07058
Bomber Code <contacto@bombercode.net>
2020-05-08 15:48:25 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/FacebookComments/langs/spanish_es.php
@@ -0,0 +1,6 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_facebook_comments_addon_color_scheme'] = 'Esquema de colores del widget';
+$txt['lp_facebook_comments_addon_color_scheme_set'] = array('blanco' => 'Blanco', 'negro' => 'Negro');
