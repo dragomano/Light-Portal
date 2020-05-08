2c509b9ce1ca9996e621fb54b0388372eb6089af
Bomber Code <contacto@bombercode.net>
2020-05-08 15:46:30 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/EasyMarkdownEditor/langs/spanish_es.php
@@ -0,0 +1,13 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['md'] = 'Markdown personalizado'; // Check meaning Markdown
+$txt['lp_block_types_descriptions']['md'] = 'En este bloque, puede usar la sintaxis básica de Markdown como contenido.';
+$txt['lp_easy_markdown_editor_description'] = 'Agrega un bloque con contenido personalizado de Markdown, así como un editor para trabajar con Markdown.';
+
+$txt['lp_easy_markdown_editor_addon_tasks'] = 'Lista de tareas';
+$txt['lp_easy_markdown_editor_addon_lines'] = 'líneas: ';
+$txt['lp_easy_markdown_editor_addon_words'] = 'palabras: ';
+$txt['lp_easy_markdown_editor_addon_toggle'] = 'Alternar lado a lado';
+$txt['lp_easy_markdown_editor_addon_guide'] = 'Guía Markdown';
