ccd82f44071af9192ae29f0b860d547481999379
Bomber Code <contacto@bombercode.net>
2020-05-08 22:33:23 -0300

    Create spanish_latin.php

---
+++ Sources/LightPortal/addons/WhosOnline/langs/spanish_latin.php
@@ -0,0 +1,8 @@
+<?php
+// .spanish latin language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['whos_online'] = 'Quien esta en linea';
+$txt['lp_block_types_descriptions']['whos_online'] = 'Muestra una lista de usuarios en línea.';
+
+$txt['lp_whos_online_addon_update_interval'] = 'Intervalo de actualización de la lista, en segundos.';
