df81e251df0b96861c07d326adc55716f313a695
Bomber Code <contacto@bombercode.net>
2020-05-08 21:54:24 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/Todays/langs/spanish_es.php
@@ -0,0 +1,10 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['todays'] = 'Hoy';
+$txt['lp_block_types_descriptions']['todays'] = 'Muestra el calendario, cumpleaños, días festivos o eventos de hoy.';
+
+$txt['lp_todays_addon_type'] = 'Que mostrar';
+$txt['lp_todays_addon_type_set'] = array('cumpleaños' => 'Cumpleaños', 'días festivos' => 'Días Festivos', 'eventos' => 'Eventos', 'calendario' => 'Calendario');
+$txt['lp_todays_addon_empty_list'] = 'No hay nada hoy.';
