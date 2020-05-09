172d0ca78a5acae9d5f5b6235bd0e7b84a5f033e
Bomber Code <contacto@bombercode.net>
2020-05-08 21:47:09 -0300

    Create spanish_latin.php

---
+++ Sources/LightPortal/addons/TagList/langs/spanish_latin.php
@@ -0,0 +1,12 @@
+<?php
+// .spanish latin language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['tag_list'] = 'Lista de etiquetas';
+$txt['lp_block_types_descriptions']['tag_list'] = 'Muestra una lista de etiquetas de las páginas del portal.';
+
+$txt['lp_tag_list_addon_source'] = 'Fuente de etiqueta';
+$txt['lp_tag_list_addon_source_set'] = array(
+	'lp_tags'  => 'Etiquetas de la página del portal',
+	'keywords' => 'Tema de palabras claves'
+);
