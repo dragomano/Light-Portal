58d1ed734a8bdd16f935aef9666d4e12833db56b
Bomber Code <contacto@bombercode.net>
2020-05-08 22:15:36 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/Trumbowyg/langs/spanish_es.php
@@ -0,0 +1,5 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_trumbowyg_description'] = 'Permite un editor visual para trabajar con contenido HTML.';
