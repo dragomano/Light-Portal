5a9b253f728da8e07ecb76e7511f6a26cafc1f7f
Bomber Code <contacto@bombercode.net>
2020-05-08 22:05:30 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/TopTopics/langs/spanish_es.php
@@ -0,0 +1,11 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['top_topics'] = 'Temas principales';
+$txt['lp_block_types_descriptions']['top_topics'] = 'Muestra una lista de temas populares.';
+
+$txt['lp_top_topics_addon_type'] = 'Cálculo de la popularidad.';
+$txt['lp_top_topics_addon_type_set'] = array('respuestas' => 'Por respuestas', 'vistas' => 'Por vistas');
+$txt['lp_top_topics_addon_num_topics'] = 'Número de temas para mostrar';
+$txt['lp_top_topics_addon_show_numbers_only'] = 'Solo mostrar números';
