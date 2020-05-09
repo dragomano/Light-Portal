efdc43e0cb59058249a774d764d98f977d9a5879
Bomber Code <contacto@bombercode.net>
2020-05-08 21:38:27 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/RecentPosts/langs/spanish_es.php
@@ -0,0 +1,12 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['recent_posts'] = 'Publicaciones recientes';
+$txt['lp_block_types_descriptions']['recent_posts'] = 'Muestra una lista de publicaciones recientes en el foro.';
+
+$txt['lp_recent_posts_addon_num_posts'] = 'Número de publicaciones para mostrar';
+$txt['lp_recent_posts_addon_type'] = 'Lo que desea mostrar como un enlace a una publicacion';
+$txt['lp_recent_posts_addon_type_set'] = array('enlace' => 'Asunto del mensaje', 'avance' => 'Vista previa del mensaje');
+$txt['lp_recent_posts_addon_show_avatars'] = 'Mostrar los avatares de los usuarios';
+$txt['lp_recent_posts_addon_update_interval'] = 'Intervalo de actualización de la lista, en segundos.';
