5a4f594f0ea3f1e828da29e2c21e39118ebe853a
Bomber Code <contacto@bombercode.net>
2020-05-08 21:32:55 -0300

    Create spanish_latin.php

---
+++ Sources/LightPortal/addons/RecentAttachments/langs/spanish_latin.php
@@ -0,0 +1,12 @@
+<?php
+// .spanish latin language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['recent_attachments'] = 'Adjuntos recientes';
+$txt['lp_block_types_descriptions']['recent_attachments'] = 'Muestra los archivos adjuntos recientes que el usuario puede ver.';
+
+$txt['lp_recent_attachments_addon_num_attachments'] = 'Número de archivos adjuntos para mostrar';
+$txt['lp_recent_attachments_addon_extensions'] = 'Las extensiones de los archivos adjuntos que desea mostrar';
+$txt['lp_recent_attachments_addon_extensions_subtext'] = 'Ingrese las extensiones requeridas, separadas por comas. Por ejemplo: <strong>jpg,png,gif</strong>';
+$txt['lp_recent_attachments_addon_direction'] = 'Dirección de la lista';
+$txt['lp_recent_attachments_addon_direction_set'] = array('horizontal' => 'Horizontal', 'vertical' => 'Vertical');
