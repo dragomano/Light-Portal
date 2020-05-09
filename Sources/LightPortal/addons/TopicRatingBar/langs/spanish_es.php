327b54de0f283a25667b2016bece9281eeb767b7
Bomber Code <contacto@bombercode.net>
2020-05-08 22:02:13 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/TopicRatingBar/langs/spanish_es.php
@@ -0,0 +1,5 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_topic_rating_bar_description'] = 'Proporciona integración con el mod <a href="https://custom.simplemachines.org/mods/index.php?mod=3236" target="_blank" rel="noopener">Topic Rating Bar</a>, que muestra la calificación de los temas (si existe) en modo artículos en la página principal.';
