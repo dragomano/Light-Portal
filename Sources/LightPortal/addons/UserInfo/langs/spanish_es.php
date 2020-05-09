9c1ec894eccd1333db0c33e21bbe0403b728ccfd
Bomber Code <contacto@bombercode.net>
2020-05-08 22:31:36 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/UserInfo/langs/spanish_es.php
@@ -0,0 +1,6 @@
+<?php
+// .spanish latin language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['user_info'] = 'Información del usuario';
+$txt['lp_block_types_descriptions']['user_info'] = 'Muestra una información sobre el usuario actual.';
