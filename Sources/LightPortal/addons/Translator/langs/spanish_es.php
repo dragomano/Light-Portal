53fdcb9ae0d1309d509459830cfec5f6260c3638
Bomber Code <contacto@bombercode.net>
2020-05-08 21:59:48 -0300

    Create spanish_es.php

---
+++ Sources/LightPortal/addons/Translator/langs/spanish_es.php
@@ -0,0 +1,11 @@
+<?php
+// .spanish es language file Rev. 1.1
+// Spanish translation by Rock Lee https://www.bombercode.net
+
+$txt['lp_block_types']['translator'] = 'Widget de traductor';
+$txt['lp_block_types_descriptions']['translator'] = 'Muestra el botón para traducir la página actual al idioma seleccionado.';
+
+$txt['lp_translator_addon_engine'] = 'Motor usado';
+$txt['lp_translator_addon_engine_set'] = array('google' => 'Google.Traductor', 'yandex' => 'Yandex.Traductor');
+$txt['lp_translator_addon_widget_theme'] = 'Diseño del widget';
+$txt['lp_translator_addon_auto_mode'] = 'Traducción automática';
