ecec9fdcade39ad5b8f0af7edbf7238e3303c12b
Bugo <bugo@dragomano.ru>
2020-06-05 21:50:19 +0500

    Add HidingBlocks addon

---
+++ Sources/LightPortal/addons/HidingBlocks/langs/ukrainian.php
@@ -0,0 +1,12 @@
+<?php
+
+$txt['lp_hiding_blocks_description'] = 'Дає можливість приховувати будь-який блок залежно від ширини екрану.';
+
+$txt['lp_hiding_blocks_addon_hidden_breakpoints']     = 'Приховувати блок на екранах із заданою шириною';
+$txt['lp_hiding_blocks_addon_hidden_breakpoints_set'] = array(
+	'xs' => 'менше 576px',
+	'sm' => 'от 576 до 768px',
+	'md' => 'от 768 до 992px',
+	'lg' => 'от 992 до 1200px',
+	'xl' => 'більше 1200px'
+);
