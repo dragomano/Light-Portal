c0fa70fc213ce57afb1e32e5f9fafac11b69fa4b
Bugo <bugo@dragomano.ru>
2020-06-13 13:06:41 +0500

    Add some options for Trumbowyg addon

--- Sources/LightPortal/addons/Trumbowyg/langs/english.php
+++ Sources/LightPortal/addons/Trumbowyg/langs/english.php
@@ -1,3 +1,7 @@
 <?php
 
 $txt['lp_trumbowyg_description'] = 'Enables a visual editor for working with HTML content.';
+
+$txt['lp_trumbowyg_addon_dark_themes']   = 'Select themes for which the editor appearance should be dark';
+$txt['lp_trumbowyg_addon_auto_grow']     = 'Extend editor area';
+$txt['lp_trumbowyg_addon_auto_grow_set'] = array($txt['no'], 'When writing a long text', 'When editor get focus and reduce on blur');
--- Sources/LightPortal/addons/Trumbowyg/langs/russian.php
+++ Sources/LightPortal/addons/Trumbowyg/langs/russian.php
@@ -1,3 +1,7 @@
 <?php
 
 $txt['lp_trumbowyg_description'] = 'Подключает визуальный редактор для работы с HTML-контентом.';
+
+$txt['lp_trumbowyg_addon_dark_themes']   = 'Отметьте темы, при выборе которых оформление редактора должно быть тёмным';
+$txt['lp_trumbowyg_addon_auto_grow']     = 'Расширять область редактора';
+$txt['lp_trumbowyg_addon_auto_grow_set'] = array($txt['no'], 'При работе с большими текстами', 'При получении фокуса');
