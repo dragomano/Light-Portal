81d817101b900a143470cd0ac813dbc5c0ee79c7
Bugo <bugo@dragomano.ru>
2020-06-13 12:59:59 +0500

    Add fullscreen mode for CodeMirror addon

--- Sources/LightPortal/addons/CodeMirror/langs/english.php
+++ Sources/LightPortal/addons/CodeMirror/langs/english.php
@@ -1,3 +1,4 @@
 <?php
 
 $txt['lp_code_mirror_description'] = 'Provides syntax highlighting when editing pages and blocks with PHP content.';
+$txt['lp_code_mirror_editor_hint'] = '<strong>F11</strong> — open the editor in full screen, <strong>ESC</strong> — return to default view';
--- Sources/LightPortal/addons/CodeMirror/langs/russian.php
+++ Sources/LightPortal/addons/CodeMirror/langs/russian.php
@@ -1,3 +1,4 @@
 <?php
 
 $txt['lp_code_mirror_description'] = 'Обеспечивает подсветку синтаксиса при редактировании страниц и блоков с PHP-контентом.';
+$txt['lp_code_mirror_editor_hint'] = '<strong>F11</strong> — открыть редактор во весь экран, <strong>ESC</strong> — вернуться к виду по умолчанию';
