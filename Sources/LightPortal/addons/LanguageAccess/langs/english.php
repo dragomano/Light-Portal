a1b082d158788754d7cbcc961163ed738870c33e
Bugo <bugo@dragomano.ru>
2020-06-24 11:46:15 +0500

    Add LanguageAccess addon

---
+++ Sources/LightPortal/addons/LanguageAccess/langs/english.php
@@ -0,0 +1,6 @@
+<?php
+
+$txt['lp_language_access_description'] = 'Allows you to display a specific block depending on the user\'s language.';
+
+$txt['lp_language_access_addon_allowed_languages']         = 'Display a block if the user\'s current language is';
+$txt['lp_language_access_addon_allowed_languages_subtext'] = 'If no language is selected, there are no restrictions.';
