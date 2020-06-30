70313f7cdfdffd0bbaee6486a88c13d7c646333b
Adrek <spam@smf.net.pl>
2020-06-30 23:45:07 +0200

    Update polish.php

--- Sources/LightPortal/addons/Trumbowyg/langs/polish.php
+++ Sources/LightPortal/addons/Trumbowyg/langs/polish.php
@@ -1,3 +1,7 @@
 <?php
 
 $txt['lp_trumbowyg_description'] = 'Włącza wizualny edytor zawartości HTML.';
+
+$txt['lp_trumbowyg_addon_dark_themes']   = 'Wybierz style w których edytor będzie używał styl ciemny';
+$txt['lp_trumbowyg_addon_auto_grow']     = 'Rozszerz edytor';
+$txt['lp_trumbowyg_addon_auto_grow_set'] = array($txt['no'], 'Podczas pisania długiego tekstu', 'Gdy edytor jest aktywny');
