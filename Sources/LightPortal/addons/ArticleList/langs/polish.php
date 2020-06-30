f8e087dc4d4718a4dd0ed0d20504e7c2f2a5e03e
Adrek <spam@smf.net.pl>
2020-06-30 23:15:01 +0200

    Create polish.php

---
+++ Sources/LightPortal/addons/ArticleList/langs/polish.php
@@ -0,0 +1,15 @@
+<?php
+
+$txt['lp_block_types']['article_list']              = 'Lista artykułów';
+$txt['lp_block_types_descriptions']['article_list'] = 'Wyświetla listę wybranych artykułów portalu (z obrazami jeśli są dostępne).';
+
+$txt['lp_article_list_addon_body_class']       = 'Klasa CSS bloku artykułów';
+$txt['lp_article_list_addon_article_type']     = 'Wyświetlanie';
+$txt['lp_article_list_addon_article_type_set'] = array('Wątki', 'Strony portalu');
+$txt['lp_article_list_addon_ids']              = 'ID wątków lub stron portalu ';
+$txt['lp_article_list_addon_ids_subtext']      = 'Podaj identyfikatory wątków lub stron portalu oddzielone przecinkiem.';
+$txt['lp_article_list_addon_direction']        = 'Kierunek listy';
+$txt['lp_article_list_addon_direction_set']    = array('horizontal' => 'Poziomy', 'vertical' => 'Pionowy');
+$txt['lp_article_list_addon_seek_images']      = 'Szukaj obrazów w treści artykułów';
+
+$txt['lp_article_list_addon_no_items'] = 'Nie dodano artykułów.';
