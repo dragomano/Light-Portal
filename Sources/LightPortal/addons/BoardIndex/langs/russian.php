e63abb4c8432dbe91d13a6d61d324670a28ea3be
Bugo <bugo@dragomano.ru>
2020-09-01 09:52:04 +0500

    Add BoardIndex addon

---
+++ Sources/LightPortal/addons/BoardIndex/langs/russian.php
@@ -0,0 +1,5 @@
+<?php
+
+$txt['lp_board_index_description'] = 'Тонкая настройка главной страницы форума (<em>?action=forum</em>), если включен портал.';
+
+$txt['lp_board_index_addon_allow_for_spiders'] = 'Разрешить индексацию главной страницы форума';
