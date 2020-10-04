996da9c06936e1636bb307e51bc7a5613fd7e976
Bugo <bugo@dragomano.ru>
2020-10-04 23:36:02 +0500

    Add FrontpageFlarumStyle addon

---
+++ Sources/LightPortal/addons/FrontpageFlarumStyle/FrontpageFlarumStyle.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Bugo\LightPortal\Addons\FrontpageFlarumStyle;
+
+/**
+ * FrontpageFlarumStyle
+ *
+ * @package Light Portal
+ * @link https://dragomano.ru/mods/light-portal
+ * @author Bugo <bugo@dragomano.ru>
+ * @copyright 2019-2020 Bugo
+ * @license https://spdx.org/licenses/GPL-3.0-or-later.html GPL-3.0-or-later
+ *
+ * @version 1.1
+ */
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+class FrontpageFlarumStyle
+{
+
+	/**
+	 * Specifying the addon type (if 'block', you do not need to specify it)
+	 *
+	 * Указываем тип аддона (если 'block', то можно не указывать)
+	 *
+	 * @var string
+	 */
+	public static $addon_type = 'other';
+
+	/**
+	 * Load custom template for frontpage topics
+	 *
+	 * Загружаем пользовательский шаблон для статей-тем
+	 *
+	 * @return void
+	 */
+	public static function frontpageCustomTemplate()
+	{
+		global $modSettings, $context;
+
+		if (empty($modSettings['lp_frontpage_mode']) || $modSettings['lp_frontpage_mode'] != 2)
+			return;
+
+		$context['lp_all_categories'] = self::getListSelectedBoards();
+
+		require_once(__DIR__ . '/Template.php');
+
+		$context['sub_template'] = 'show_topics_as_flarum_style';
+	}
+
+	/**
+	 * Get the list of categories with boards, considering the selected boards in the portal settings
+	 *
+	 * Получаем список всех категорий с разделами, учитывая отмеченные разделы в настройках портала
+	 *
+	 * @return array
+	 */
+	private static function getListSelectedBoards()
+	{
+		global $sourcedir, $modSettings;
+
+		require_once($sourcedir . '/Subs-MessageIndex.php');
+
+		$boardListOptions = array(
+			'ignore_boards'   => true,
+			'use_permissions' => true,
+			'not_redirection' => true,
+			'included_boards' => !empty($modSettings['lp_frontpage_boards']) ? explode(',', $modSettings['lp_frontpage_boards']) : []
+		);
+
+		return getBoardList($boardListOptions);
+	}
+}
---
+++ Sources/LightPortal/addons/FrontpageFlarumStyle/langs/russian.php
@@ -0,0 +1,6 @@
+<?php
+
+$txt['lp_frontpage_flarum_style_description'] = 'Оформление списка статей на главной странице портала в стиле движка Flarum.<br>Работает, если выбран режим «Первая страница портала — Все темы из выбранных разделов».';
+
+$txt['lp_frontpage_flarum_style_addon_all_boards'] = 'Все разделы';
+$txt['lp_frontpage_flarum_style_addon_tags']       = 'Теги';
