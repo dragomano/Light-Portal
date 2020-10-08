91c09009bb761d5bbd88e9807a07d2983aad0e72
Bugo <bugo@dragomano.ru>
2020-10-08 17:31:08 +0500

    Update the News addon

--- Sources/LightPortal/addons/News/News.php
+++ Sources/LightPortal/addons/News/News.php
@@ -31,18 +31,109 @@ class News
 	public static $addon_icon = 'far fa-newspaper';
 
 	/**
-	 * Get the news list of the forum
+	 * The news index to be displayed (0 - random news)
 	 *
-	 * Получаем список новостей форума
+	 * Порядковый номер новости для отображения (0 - случайная новость)
+	 *
+	 * @var string
+	 */
+	private static $selected_item = 0;
+
+	/**
+	 * Adding the block options
+	 *
+	 * Добавляем параметры блока
+	 *
+	 * @param array $options
+	 * @return void
+	 */
+	public static function blockOptions(&$options)
+	{
+		$options['news']['parameters']['selected_item'] = static::$selected_item;
+	}
+
+	/**
+	 * Validate options
+	 *
+	 * Валидируем параметры
+	 *
+	 * @param array $parameters
+	 * @param string $type
+	 * @return void
+	 */
+	public static function validateBlockData(&$parameters, $type)
+	{
+		global $context;
+
+		if ($type !== 'news')
+			return;
+
+		$parameters['selected_item'] = FILTER_VALIDATE_INT;
+	}
+
+	/**
+	 * Adding fields specifically for this block
+	 *
+	 * Добавляем поля конкретно для этого блока
+	 *
+	 * @return void
+	 */
+	public static function prepareBlockFields()
+	{
+		global $context, $txt;
+
+		if ($context['lp_block']['type'] !== 'news')
+			return;
+
+		$context['posting_fields']['selected_item']['label']['text'] = $txt['lp_news_addon_selected_item'];
+		$context['posting_fields']['selected_item']['input'] = array(
+			'type' => 'select',
+			'attributes' => array(
+				'id' => 'selected_item'
+			),
+			'options' => array(),
+			'tab' => 'content'
+		);
+
+		self::getData();
+
+		$news = [$txt['lp_news_addon_random_news']];
+		if (!empty($context['news_lines'])) {
+			array_unshift($context['news_lines'], $txt['lp_news_addon_random_news']);
+			$news = $context['news_lines'];
+		}
+
+		foreach ($news as $key => $value) {
+			if (RC2_CLEAN) {
+				$context['posting_fields']['selected_item']['input']['options'][$value]['attributes'] = array(
+					'value'    => $key,
+					'selected' => $key == $context['lp_block']['options']['parameters']['selected_item']
+				);
+			} else {
+				$context['posting_fields']['selected_item']['input']['options'][$value] = array(
+					'value'    => $key,
+					'selected' => $key == $context['lp_block']['options']['parameters']['selected_item']
+				);
+			}
+		}
+	}
+
+	/**
+	 * Get the forum news
+	 *
+	 * Получаем новость форума
 	 *
 	 * @return string
 	 */
-	public static function getData()
+	public static function getData($item = 0)
 	{
-		global $boarddir;
+		global $boarddir, $context;
 
 		require_once($boarddir . '/SSI.php');
 		setupThemeContext();
+
+		if ($item > 0)
+			return $context['news_lines'][$item - 1];
 
 		return ssi_news('return');
 	}
@@ -56,16 +147,17 @@ class News
 	 * @param string $type
 	 * @param int $block_id
 	 * @param int $cache_time
+	 * @param array $parameters
 	 * @return void
 	 */
-	public static function prepareContent(&$content, $type, $block_id, $cache_time)
+	public static function prepareContent(&$content, $type, $block_id, $cache_time, $parameters)
 	{
 		global $txt;
 
 		if ($type !== 'news')
 			return;
 
-		$news = Helpers::getFromCache('news_addon_b' . $block_id, 'getData', __CLASS__, $cache_time);
+		$news = self::getData($parameters['selected_item']);
 
 		ob_start();
 		echo $news ?: $txt['lp_news_addon_no_items'];
