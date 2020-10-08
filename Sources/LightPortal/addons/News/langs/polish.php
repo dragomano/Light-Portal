5e8bbc18ae3ee8545a107a90d78fcb349686cef7
Bugo <bugo@dragomano.ru>
2020-10-08 17:31:08 +0500

    Update the News addon

--- Sources/LightPortal/addons/News/langs/polish.php
+++ Sources/LightPortal/addons/News/langs/polish.php
@@ -1,6 +1,9 @@
 <?php
 
 $txt['lp_block_types']['news']              = 'Aktualności';
-$txt['lp_block_types_descriptions']['news'] = 'Wyświetla losową linię aktualności.';
+$txt['lp_block_types_descriptions']['news'] = 'Displays one of the forum\'s news items.';
+
+$txt['lp_news_addon_selected_item'] = 'What to display';
+$txt['lp_news_addon_random_news']   = 'The random news';
 
 $txt['lp_news_addon_no_items'] = 'Brak aktualności.';
--- Sources/LightPortal/addons/News/langs/russian.php
+++ Sources/LightPortal/addons/News/langs/russian.php
@@ -1,6 +1,9 @@
 <?php
 
 $txt['lp_block_types']['news']              = 'Новости форума';
-$txt['lp_block_types_descriptions']['news'] = 'Отображает одну из новостей форума (рандомно).';
+$txt['lp_block_types_descriptions']['news'] = 'Отображает одну из новостей форума.';
+
+$txt['lp_news_addon_selected_item'] = 'Что отображать';
+$txt['lp_news_addon_random_news']   = 'Случайную новость';
 
 $txt['lp_news_addon_no_items'] = 'Новостей пока нет.';
