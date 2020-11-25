fc51b322bc266a31401089b30c49b6ea03ab0850
Bugo <bugo@dragomano.ru>
2020-11-25 15:59:38 +0500

    Add a new addon - TinyPortal (import pages)

---
+++ Sources/LightPortal/addons/TinyPortal/langs/english.php
@@ -0,0 +1,8 @@
+<?php
+
+$txt['lp_tiny_portal_description'] = 'Support for importing TinyPortal pages (if they are exist in the database).';
+
+$txt['lp_tiny_portal_addon_label_name']  = 'Import from TinyPortal';
+$txt['lp_tiny_portal_addon_description'] = 'Import TinyPortal pages (if they are exist in the database).';
+$txt['lp_tiny_portal_addon_button_run']  = 'Import selected';
+$txt['lp_tiny_portal_addon_button_all']  = 'Import all';
---
+++ Sources/LightPortal/addons/TinyPortal/langs/ukrainian.php
@@ -0,0 +1,8 @@
+<?php
+
+$txt['lp_tiny_portal_description'] = 'Підтримка імпорту сторінок TinyPortal (якщо вони є в базі даних).';
+
+$txt['lp_tiny_portal_addon_label_name']  = 'Імпорт з TinyPortal';
+$txt['lp_tiny_portal_addon_description'] = 'Імпорт сторінок TinyPortal (якщо вони є в базі даних).';
+$txt['lp_tiny_portal_addon_button_run']  = 'Імпортувати виділені';
+$txt['lp_tiny_portal_addon_button_all']  = 'Імпортувати всі';
