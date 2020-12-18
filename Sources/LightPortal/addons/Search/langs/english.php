9908d66ac689e10203d6b9ab1c99d2aaab26d027
Bugo <bugo@dragomano.ru>
2020-12-18 13:17:53 +0500

    Add a new addon - Search

---
+++ Sources/LightPortal/addons/Search/langs/english.php
@@ -0,0 +1,6 @@
+<?php
+
+$txt['lp_block_types']['search']              = 'Search in pages';
+$txt['lp_block_types_descriptions']['search'] = 'Displays the search field for the content of portal pages.';
+
+$txt['lp_search_addon_dark_themes'] = 'Select themes for which the background of the quick search results list should be dark';
