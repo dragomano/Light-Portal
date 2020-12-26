9b0a46f339ef9bbe215b1914aaa89322aa70d1bd
Bugo <bugo@dragomano.ru>
2020-12-26 14:40:47 +0500

    Add an example of custom template for page articles

--- Themes/default/LightPortal/ViewFrontPage.template.php
+++ Themes/default/LightPortal/ViewFrontPage.template.php
@@ -350,3 +350,128 @@ function template_show_boards_as_articles()
 	<div class="infobox">', $txt['lp_no_items'], '</div>';
 	}
 }
+
+/**
+ * Кастомный вывод 5 страниц
+ *
+ * @return void
+ */
+function template_show_five_pages_as_articles()
+{
+	global $context, $scripturl, $txt, $modSettings;
+
+	if (!empty($context['lp_frontpage_articles'])) {
+		if (empty($context['lp_active_blocks']))
+			echo '
+	<div class="col-xs">';
+
+		$pages = array_slice($context['lp_frontpage_articles'], 0, 5);
+		$areas = [
+			'1/1/3/3',
+			'1/3',
+			'1/4',
+			'2/3',
+			'2/4'
+		];
+
+		echo '
+	<div class="lp_frontpage_articles" style="display: grid; margin-bottom: 2em; grid-gap: 2%; grid-template-rows: min-content; align-items: start; grid-template-columns: repeat(4, 1fr);">';
+
+		foreach ($pages as $count => $page) {
+			echo '
+		<div style="grid-area: ', $areas[$count], empty($count) ? '; height: 100%' : '', '">
+			<article class="card roundframe" style="height: 100%; margin: 0">
+				<div class="card-info-hover">';
+
+			if ($page['can_edit']) {
+				echo '
+					<div class="card-edit-icon">
+						<a href="', $scripturl, '?action=admin;area=lp_pages;sa=edit;id=', $page['id'], '">
+							<i class="fas fa-edit" title="', $txt['edit'], '"></i>
+						</a>
+					</div>';
+			}
+
+			echo '
+				</div>';
+
+			if (!empty($page['image'])) {
+				echo '
+				<div class="card-img" style="background-image: url(\'' . $page['image'] . '\')"></div>
+				<a href="', $page['link'], '">
+					<div class="card-img-hover" style="background-image: url(\'', $page['image'], '\')', empty($count) ? '; height: 100%; opacity: .3' : '', '"></div>
+				</a>';
+			}
+
+			echo '
+				<div class="card-info">
+					<span class="card-date smalltext">
+						', $page['is_new'] ? ('<span class="new_posts">' . $txt['new'] . '</span>') : '', '
+						<time datetime="', $page['datetime'], '">', $page['date'], '</time>
+					</span>
+					<h3 class="card-title">
+						<a href="', $page['link'], '">', $page['title'], '</a>
+					</h3>';
+
+			if (empty($count)) {
+				echo '
+					<p>', $page['teaser'], '</p>';
+			}
+
+			echo '
+					<div>';
+
+			if (!empty($modSettings['lp_show_author'])) {
+				echo '
+
+						<span class="card-by">';
+
+				if (empty($modSettings['lp_frontpage_article_sorting']) && !empty($page['num_comments'])) {
+					echo '
+							<i class="fas fa-reply"></i>';
+				}
+
+				if (!empty($page['author_id']) && !empty($page['author_name'])) {
+					echo '
+							<a href="', $page['author_link'], '" class="card-author">', $page['author_name'], '</a>';
+				} else {
+					echo '
+							<span class="card-author">', $txt['guest_title'], '</span>';
+				}
+
+				echo '
+						</span>';
+			}
+
+			if (!empty($modSettings['lp_show_num_views_and_comments'])) {
+				echo '
+						<span class="floatright">
+							<i class="fas fa-eye" title="', $txt['views'], '"></i> ', $page['num_views'];
+
+				if (!empty($page['num_comments'])) {
+					echo '
+							<i class="fas fa-comment" title="', $txt['lp_comments'], '"></i> ', $page['num_comments'];
+				}
+
+				echo '
+						</span>';
+			}
+
+			echo '
+					</div>
+				</div>
+			</article>
+		</div>';
+		}
+
+		echo '
+	</div>';
+
+		if (empty($context['lp_active_blocks']))
+			echo '
+	</div>';
+	} else {
+		echo '
+	<div class="infobox">', $txt['lp_no_items'], '</div>';
+	}
+}
