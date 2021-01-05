9e0c67857ac22ab4e6f227590101d4dfdfa7c6d1
Bugo <bugo@dragomano.ru>
2021-01-05 18:20:21 +0500

    Add an alternate example of custom view for topics

--- Themes/default/LightPortal/ViewFrontPage.template.php
+++ Themes/default/LightPortal/ViewFrontPage.template.php
@@ -352,6 +352,122 @@ function template_show_boards_as_articles()
 }
 
 /**
+ * Example of custom view for front topics
+ *
+ * Пример альтернативного отображения тем
+ *
+ * @return void
+ */
+function template_alt_show_topics_as_articles()
+{
+	global $context, $scripturl, $txt, $modSettings;
+
+	if (!empty($context['lp_frontpage_articles'])) {
+		if (empty($context['lp_active_blocks']))
+			echo '
+	<div class="col-xs">';
+
+		echo '
+	<div class="lp_frontpage_articles row"', !empty($context['lp_active_blocks']) ? ' style="margin-top: -10px"' : '', '>';
+
+		foreach ($context['lp_frontpage_articles'] as $topic) {
+			echo '
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-', $context['lp_frontpage_layout'], ' col-xl-', $context['lp_frontpage_layout'], '">
+			<article style="transition: all .4s cubic-bezier(.175, .885, 0, 1); position: relative; padding: 0; display: flex; flex-direction: column; justify-content: space-between; height: 96%">
+				<div class="title_bar article_header" style="padding: 8px 12px">
+					<h3 class="floatleft">
+						<a href="', $topic['msg_link'], '">', $topic['subject'], '</a>', $topic['is_new'] ? (' <span class="new_posts">' . $txt['new'] . '</span>') : '', '
+					</h3>';
+
+			if ($topic['can_edit']) {
+				echo '
+					<span class="floatright">
+						<a href="', $scripturl, '?action=post;msg=', $topic['id_msg'], ';topic=', $topic['id'], '.0">
+							<i class="fas fa-edit" title="', $txt['edit'], '"></i>
+						</a>
+					</span>';
+			}
+
+			echo '
+				</div>
+				<div class="article_body roundframe" style="overflow: hidden">';
+
+			if (!empty($modSettings['lp_show_num_views_and_comments'])) {
+				echo '
+					<span class="floatleft">
+						<i class="fas fa-eye" title="', $txt['lp_views'], '"></i> ', $topic['num_views'];
+
+				if (!empty($topic['num_replies'])) {
+					echo '
+						<i class="fas fa-comment" title="', $txt['lp_replies'], '"></i> ', $topic['num_replies'];
+				}
+
+				echo '
+					</span>';
+			}
+
+			if (!empty($topic['image'])) {
+				echo '
+					<img src="' . $topic['image'] . '" alt="">';
+			}
+
+			if (!empty($modSettings['lp_show_teaser']) && !empty($topic['teaser'])) {
+				echo '
+					<p>', $topic['teaser'], '</p>';
+			}
+
+			echo '
+					<div class="article_footer">
+						<div class="centertext" style="padding: 4px">
+							<a class="bbc_link" href="', $topic['link'], '">', $txt['lp_read_more'], '</a>
+						</div>
+						<div class="centertext" style="padding: 4px">
+							<time datetime="', $topic['datetime'], '">', $topic['date'], '</time>';
+
+			if (!empty($modSettings['lp_show_author'])) {
+				if (empty($modSettings['lp_frontpage_article_sorting']) && !empty($topic['num_replies'])) {
+					echo '
+							<i class="fas fa-reply"></i>';
+				}
+
+				if (!empty($topic['author_id']) && !empty($topic['author_name'])) {
+					echo '
+							<a href="', $topic['author_link'], '" class="card-author">', $topic['author_name'], '</a>';
+				} else {
+					echo '
+							<span class="card-author">', $txt['guest_title'], '</span>';
+				}
+			}
+
+			echo '
+						</div>
+					</div>
+				</div>
+			</article>
+		</div>';
+		}
+
+		if (!empty($context['page_index']))
+			echo '
+		<div class="col-xs-12 centertext">
+			<div class="pagesection">
+				<div class="pagelinks">', $context['page_index'], '</div>
+			</div>
+		</div>';
+
+		echo '
+	</div>';
+
+		if (empty($context['lp_active_blocks']))
+			echo '
+	</div>';
+	} else {
+		echo '
+	<div class="infobox">', $txt['lp_no_items'], '</div>';
+	}
+}
+
+/**
  * Example of custom view for front pages
  *
  * Пример альтернативного отображения страниц
