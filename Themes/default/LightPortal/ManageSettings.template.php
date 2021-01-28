55ad5c024b31a61ff8387cbf3ab0f5097b78aba7
Bugo <bugo@dragomano.ru>
2021-01-28 21:26:59 +0500

    Add "error" class for remove icons on Categories settings

--- Themes/default/LightPortal/ManageSettings.template.php
+++ Themes/default/LightPortal/ManageSettings.template.php
@@ -91,7 +91,7 @@ function show_single_category($id, $cat)
 		<td class="centertext handle"><i class="fas fa-arrows-alt"></i></td>
 		<td>
 			<span class="floatright">
-				<span @click="category.remove($el)" title="', $txt['remove'], '">&times;</span>
+				<span @click="category.remove($el)" title="', $txt['remove'], '" class="error">&times;</span>
 			</span>
 			<label for="category_name', $id, '" class="handle">', $txt['lp_category'], '</label>
 			<input
