98f9260a6bfcb910caeb02dbc6967e1faca7d332
Bugo <bugo@dragomano.ru>
2021-02-21 22:54:47 +0500

    Add a new addon - Disqus

---
+++ Sources/LightPortal/addons/Disqus/Disqus.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Bugo\LightPortal\Addons\Disqus;
+
+/**
+ * Disqus
+ *
+ * @package Light Portal
+ * @link https://dragomano.ru/mods/light-portal
+ * @author Bugo <bugo@dragomano.ru>
+ * @copyright 2019-2021 Bugo
+ * @license https://spdx.org/licenses/GPL-3.0-or-later.html GPL-3.0-or-later
+ *
+ * @version 1.6
+ */
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+class Disqus
+{
+	/**
+	 * @var string
+	 */
+	public $addon_type = 'comment';
+
+	/**
+	 * Adding the new comment type
+	 *
+	 * Добавляем новый тип комментариев
+	 *
+	 * @return void
+	 */
+	public function init()
+	{
+		global $txt;
+
+		$txt['lp_show_comment_block_set']['disqus'] = 'Disqus';
+	}
+
+	/**
+	 * @param array $config_vars
+	 * @return void
+	 */
+	public function addSettings(&$config_vars)
+	{
+		global $txt;
+
+		$config_vars[] = array('text', 'lp_disqus_addon_shortname', 'subtext' => $txt['lp_disqus_addon_shortname_subtext']);
+	}
+
+	/**
+	 * Adding comment block
+	 *
+	 * Добавляем блок комментариев
+	 *
+	 * @return void
+	 */
+	public function comments()
+	{
+		global $modSettings, $context;
+
+		if (!empty($modSettings['lp_show_comment_block']) && $modSettings['lp_show_comment_block'] == 'disqus' && !empty($modSettings['lp_disqus_addon_shortname'])) {
+			$context['lp_disqus_comment_block'] = '
+				<div id="disqus_thread" class="windowbg"></div>
+				<script>
+					var disqus_config = function () {
+						this.page.url = "' . $context['canonical_url'] . '";
+						this.page.identifier = "' . $context['lp_page']['id'] . '";
+					};
+					(function() {
+						var d = document, s = d.createElement("script");
+						s.src = "https://' . $modSettings['lp_disqus_addon_shortname'] . '.disqus.com/embed.js";
+						s.setAttribute("data-timestamp", +new Date());
+						(d.head || d.body).appendChild(s);
+					})();
+				</script>';
+		}
+	}
+}
---
+++ Sources/LightPortal/addons/Disqus/langs/english.php
@@ -0,0 +1,4 @@
+<?php
+
+$txt['lp_disqus_addon_shortname']         = 'Website shortname';
+$txt['lp_disqus_addon_shortname_subtext'] = 'This is used to uniquely identify your website on Disqus.<br>Copy your site <strong>shortname</strong> from <a class="bbc_link" href="https://disqus.com/admin/settings/general/" target="_blank" rel="noopener">the Disqus settings</a> and paste here.';
