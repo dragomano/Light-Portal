08f151c913bc6001318d3fef5f9680b5d534fce5
Bugo <bugo@dragomano.ru>
2021-02-23 15:17:46 +0500

    Add a new addon - InstagramFeed

---
+++ Sources/LightPortal/addons/InstagramFeed/langs/english.php
@@ -0,0 +1,22 @@
+<?php
+
+$txt['lp_block_types']['instagram_feed']              = 'Instagram Feed';
+$txt['lp_block_types_descriptions']['instagram_feed'] = 'Displays the Instagram feed.';
+
+$txt['lp_instagram_feed_addon_username']                  = 'Instagram username';
+$txt['lp_instagram_feed_addon_username_subtext']          = 'Required if tag is not defined.';
+$txt['lp_instagram_feed_addon_tag']                       = 'Instagram tag';
+$txt['lp_instagram_feed_addon_tag_subtext']               = 'Required if username is not defined.';
+$txt['lp_instagram_feed_addon_display_profile']           = 'Enables displaying the profile';
+$txt['lp_instagram_feed_addon_display_biography']         = 'Enables displaying the biography';
+$txt['lp_instagram_feed_addon_display_biography_subtext'] = 'Not compatible when loading a tag feed.';
+$txt['lp_instagram_feed_addon_display_gallery']           = 'Enables displaying the gallery';
+$txt['lp_instagram_feed_addon_display_captions']          = 'Enables displaying captions for each post as overlay on hover';
+$txt['lp_instagram_feed_addon_display_igtv']              = 'Enables displaying the IGTV feed if available';
+$txt['lp_instagram_feed_addon_display_igtv_subtext']      = $txt['lp_instagram_feed_addon_display_biography_subtext'];
+$txt['lp_instagram_feed_addon_items']                     = 'Number of items to display';
+$txt['lp_instagram_feed_addon_items_subtext']             = 'Up to 12 for users, up to 72 for tags.';
+$txt['lp_instagram_feed_addon_items_per_row']             = 'Number of items that will be displayed for each row';
+$txt['lp_instagram_feed_addon_margin']                    = 'Margin between items in gallery';
+$txt['lp_instagram_feed_addon_image_size']                = 'Native resolution of the images that will be displayed in the gallery';
+$txt['lp_instagram_feed_addon_image_size_subtext']        = 'Does not apply to video previews.';
---
+++ Sources/LightPortal/addons/InstagramFeed/langs/ukrainian.php
@@ -0,0 +1,22 @@
+<?php
+
+$txt['lp_block_types']['instagram_feed']              = 'Стрічка Instagram';
+$txt['lp_block_types_descriptions']['instagram_feed'] = 'Відображає стрічку Instagram.';
+
+$txt['lp_instagram_feed_addon_username']                  = 'Ім\'я користувача Instagram';
+$txt['lp_instagram_feed_addon_username_subtext']          = 'Обов\'язково, якщо не вказано хештег.';
+$txt['lp_instagram_feed_addon_tag']                       = 'Хештег Instagram';
+$txt['lp_instagram_feed_addon_tag_subtext']               = 'Обов\'язково, якщо не вказано ім\'я користувача.';
+$txt['lp_instagram_feed_addon_display_profile']           = 'Включає відображення профілю';
+$txt['lp_instagram_feed_addon_display_biography']         = 'Включає відображення біографії';
+$txt['lp_instagram_feed_addon_display_biography_subtext'] = 'Несумісне при завантаженні стрічки хештегів.';
+$txt['lp_instagram_feed_addon_display_gallery']           = 'Включає відображення галереї';
+$txt['lp_instagram_feed_addon_display_captions']          = 'Включає відображення підписів до кожного повідомлення у вигляді накладення при наведенні курсору';
+$txt['lp_instagram_feed_addon_display_igtv']              = 'Включає відображення стрічки IGTV, якщо вона доступна';
+$txt['lp_instagram_feed_addon_display_igtv_subtext']      = $txt['lp_instagram_feed_addon_display_biography_subtext'];
+$txt['lp_instagram_feed_addon_items']                     = 'Кількість відображуваних елементів';
+$txt['lp_instagram_feed_addon_items_subtext']             = 'До 12 для користувачів, до 72 для хештегів.';
+$txt['lp_instagram_feed_addon_items_per_row']             = 'Кількість елементів для відображення в одному рядку';
+$txt['lp_instagram_feed_addon_margin']                    = 'Відступ між елементами в галереї';
+$txt['lp_instagram_feed_addon_image_size']                = 'Рідне дозвіл зображень, які будуть відображатися в галереї';
+$txt['lp_instagram_feed_addon_image_size_subtext']        = 'Не застосовується до превьюшки відео.';
