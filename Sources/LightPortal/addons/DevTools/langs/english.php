65dfef54c162b0bbeb911747ebaee8b75e4b1eb9
Bugo <bugo@dragomano.ru>
2021-04-15 16:45:09 +0500

    Update DevTools languages

--- Sources/LightPortal/addons/DevTools/langs/english.php
+++ Sources/LightPortal/addons/DevTools/langs/english.php
@@ -3,7 +3,7 @@
 $txt['lp_dev_tools_description'] = 'Tools for template and plugin developers.';
 
 $txt['lp_dev_tools_addon_show_template_switcher']  = 'Display a list of template layouts for article cards';
-$txt['lp_dev_tools_addon_fake_cards']              = 'Generating fake cards';
+$txt['lp_dev_tools_addon_fake_cards']              = 'Display fake cards';
 $txt['lp_dev_tools_addon_fake_cards_subtext']      = 'The plugin generates 6 fake cards on the frontpage, for testing templates.';
 
 $txt['lp_dev_tools_addon_template'] = 'Template';
