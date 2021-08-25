8bffd8a0487b65cd1a3f24afe57f7517339a6029
Bugo <bugo@dragomano.ru>
2021-08-25 16:22:10 +0500

    Add SimpleMenu

---
+++ Sources/LightPortal/addons/SimpleMenu/langs/russian.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'title' => 'Простое меню',
+	'description' => 'Отображает простое меню из указанных элементов.',
+	'items' => 'Пункты меню',
+	'name_placeholder' => 'Название',
+	'link_placeholder' => 'Ссылка или action',
+	'item_add' => 'Добавить пункт',
+];
