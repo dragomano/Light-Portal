b0480e853269ce551cf7756d787aa17a3cf4eaf6
Bomber Code <contacto@bombercode.net>
2021-09-25 22:29:54 -0300

    1.0

---
+++ Sources/LightPortal/addons/DevTools/langs/spanish_es.php
@@ -0,0 +1,10 @@
+<?php
+// Spanish translation by Rock Lee https://www.bombercode.net | Rev. 1.0
+
+return [
+	'description' => 'Herramientas para desarrolladores de plantillas y complementos.',
+	'show_template_switcher' => 'Mostrar una lista de diseños de plantillas para tarjetas de artículos',
+	'fake_cards' => 'Mostrar tarjetas falsas',
+	'fake_cards_subtext' => 'El complemento genera 6 tarjetas falsas en la página principal para probar las plantillas.',
+	'template' => 'Plantilla'
+];
