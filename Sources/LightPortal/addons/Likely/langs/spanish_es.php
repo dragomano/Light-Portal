eb9fde7a73264fad6c3082edfe8f2361fac08f10
Bomber Code <contacto@bombercode.net>
2021-09-25 22:09:34 -0300

    1.2

--- Sources/LightPortal/addons/Likely/langs/spanish_es.php
+++ Sources/LightPortal/addons/Likely/langs/spanish_es.php
@@ -1,12 +1,21 @@
 <?php
-// .spanish es language file Rev. 1.1
-// Spanish translation by Rock Lee https://www.bombercode.net
+// Spanish translation by Rock Lee https://www.bombercode.net | Rev. 1.2
+		
 return [
 	'title' => 'Likely',
 	'description' => 'Muestra una lista de botones para compartir en redes sociales.',
 	'size' => 'Tamaño del botón',
+	'size_set' => array(
+		'small' => 'Pequeño',
+		'big' => 'Grande'
+	),
 	'skin' => 'Color del botón',
+	'skin_set' => array(
+		'normal' => 'Normal',
+		'light' => 'Ligero'
+	),
 	'buttons' => 'Botones que se muestran',
+	'select_buttons' => 'Seleccionar botones sociales para mostrar',
 	'buttons_set' => array(
 		'facebook' => 'Compartir',
 		'twitter' => 'Tweet',
@@ -16,7 +25,7 @@ return [
 		'telegram' => 'Enviar',
 		'linkedin' => 'Compartir',
 		'whatsapp' => 'Enviar',
-		'viber' => 'Enviar',
+		'viber' => 'Send',
 		'reddit' => 'Compartir',
 	),
 ];
