487619b4add333be351837f31b98ccf097cfdfb9
Bomber Code <contacto@bombercode.net>
2021-09-25 21:46:06 -0300

    Update 1.2

--- Sources/LightPortal/addons/AdsBlock/langs/spanish_es.php
+++ Sources/LightPortal/addons/AdsBlock/langs/spanish_es.php
@@ -1,14 +1,18 @@
 <?php
-// .spanish es language file Rev. 1.1
-// Spanish translation by Rock Lee https://www.bombercode.net
+// Spanish translation by Rock Lee https://www.bombercode.net | Rev. 1.2
+
 return [
 	'title' => 'Bloque de anuncios',
 	'description' => 'Muestra código HTML personalizado en foros y temas.',
+	'min_replies' => 'No muestre anuncios en temas que tengan menos de la cantidad de respuestas especificada',
+	'loader_code' => 'Código del cargador (entre las etiquetas &lt;head&gt; y &lt;/head&gt;)',
+	'end_time' => 'Hora de finalización (el bloque se apagará automáticamente)',
 	'ads_type' => 'Foros y temas',
 	'included_boards' => 'Foros',
 	'included_boards_subtext' => 'Ingrese la ID del foro para mostrar anuncios solo allí. De lo contrario, el bloque se mostrará en todos los foros.',
 	'included_topics' => 'Temas',
 	'included_topics_subtext' => 'Ingrese ID del tema para mostrar anuncios solo allí. De lo contrario, el bloque se mostrará en todos los temas.',
+	'select_placement' => '¿Dónde debería mostrarse el bloque?',
 	'placement_set' => array(
 		'En la parte superior del foro',
 		'En la parte inferior del foro',
@@ -23,4 +27,5 @@ return [
 		'Después de cada último mensaje en la página.',
 		'Después del último mensaje'
 	),
+	'no_ads_placement' => 'The <strong>placement</strong> field was not filled out. It is required.',
 ];
