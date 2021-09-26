f4d5dd7fbdd98921ae3f712ca7dc17edcdb63393
Bomber Code <contacto@bombercode.net>
2021-09-26 11:24:08 -0300

    fix word

--- Sources/LightPortal/addons/PrettyUrls/langs/spanish_es.php
+++ Sources/LightPortal/addons/PrettyUrls/langs/spanish_es.php
@@ -2,5 +2,5 @@
 // Spanish translation by Rock Lee https://www.bombercode.net | Rev. 1.2
 
 return [
-	'description' => 'Proporciona soporte para la acción del <strong>portal</strong> y páginas URL como <strong>su_foro/página/alias</strong> con el mod Pretty Urls.'
+	'description' => 'Proporciona soporte para la acción del <strong>portal</strong> y páginas URL como <strong>su_foro/page/alias</strong> con el mod Pretty Urls.'
 ];
