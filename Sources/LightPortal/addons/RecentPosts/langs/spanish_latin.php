0b58508426327893beeb287899314f227d892511
Bomber Code <contacto@bombercode.net>
2021-09-25 22:45:04 -0300

    1.2

--- Sources/LightPortal/addons/RecentPosts/langs/spanish_latin.php
+++ Sources/LightPortal/addons/RecentPosts/langs/spanish_latin.php
@@ -1,20 +1,20 @@
 <?php
-// .spanish latin language file Rev. 1.1
-// Spanish translation by Rock Lee https://www.bombercode.net
+// Spanish translation by Rock Lee https://www.bombercode.net | Rev. 1.2
+
 return [
 	'title' => 'Publicaciones recientes',
 	'description' => 'Muestra una lista de publicaciones recientes en el foro.',
 	'num_posts' => 'Número de publicaciones para mostrar',
 	'type' => 'Lo que desea mostrar como un enlace a una publicacion',
 	'type_set' => array('Asunto del mensaje', 'Vista previa del mensaje'),
-	'exclude_boards' => 'Exclude boards',
-	'exclude_boards_subtext' => 'If you specify the IDs of the boards, recent posts will NOT be displayed from these boards.',
-	'include_boards' => 'Include boards',
-	'include_boards_subtext' => 'If you specify the IDs of the boards, recent posts will be displayed ONLY from those boards.',
-	'exclude_topics' => 'Exclude topics',
-	'exclude_topics_subtext' => 'If you specify the IDs of the topics, recent posts will NOT be displayed from these topics.',
-	'include_topics' => 'Include topics',
-	'include_topics_subtext' => 'If you specify the IDs of the topics, recent posts will be displayed ONLY from those topics.',
+	'exclude_boards' => 'Excluir foros',
+	'exclude_boards_subtext' => 'Si especifica las ID de los foros, NO se mostrarán las publicaciones recientes de estos foros.',
+	'include_boards' => 'Incluir foros',
+	'include_boards_subtext' => 'Si especifica las ID de los foros, las publicaciones recientes se mostrarán ÚNICAMENTE de esos foros.',
+	'exclude_topics' => 'Excluir temas',
+	'exclude_topics_subtext' => 'Si especifica las ID de los temas, NO se mostrarán publicaciones recientes de estos temas.',
+	'include_topics' => 'Incluir temas ',
+	'include_topics_subtext' => 'Si especifica las ID de los temas, las publicaciones recientes SOLO se mostrarán de esos temas.',
 	'show_avatars' => 'Mostrar los avatares de los usuarios',
 	'update_interval' => 'Intervalo de actualización de la lista, en segundos',
 ];
