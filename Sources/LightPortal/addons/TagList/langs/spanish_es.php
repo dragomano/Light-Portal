732b452b6fdfe3ce9bc3241a5b952da7d9cf8ca1
Bomber Code <contacto@bombercode.net>
2021-09-25 22:40:34 -0300

    1.2

--- Sources/LightPortal/addons/TagList/langs/spanish_es.php
+++ Sources/LightPortal/addons/TagList/langs/spanish_es.php
@@ -1,8 +1,12 @@
 <?php
-// .spanish es language file Rev. 1.1
-// Spanish translation by Rock Lee https://www.bombercode.net
+// Spanish translation by Rock Lee https://www.bombercode.net | Rev. 1.2
+
 return [
 	'title' => 'Lista de etiquetas',
+	'description' => 'Muestra una lista de etiquetas de las páginas del portal (o temas del foro, si <a class="bbc_link" href="https://custom.simplemachines.org/mods/index.php?mod=2659" target="_blank" rel="noopener">Optimus</a> está instalado).',
 	'source' => 'Fuente de etiqueta',
 	'source_set' => array('Etiquetas de la página del portal', 'Tema de palabras claves')
+	'sorting' => 'Clasificación de etiquetas',
+	'sorting_set' => array('Por nombre', 'Por frecuencia')
+
 ];
