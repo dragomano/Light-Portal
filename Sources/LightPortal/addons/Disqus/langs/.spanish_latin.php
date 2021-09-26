996457c4413bd3ddc79fd058ef0323d8118cb1f3
Bomber Code <contacto@bombercode.net>
2021-09-25 21:36:08 -0300

    1.0

---
+++ Sources/LightPortal/addons/Disqus/langs/.spanish_latin.php
@@ -0,0 +1,6 @@
+/** Spanish translation by Rock Lee https://www.bombercode.net | Rev. 1.0 */
+ 
+ return [
+	'shortname' => 'Nombre abreviado del sitio web',
+	'shortname_subtext' => 'Esto se utiliza para identificar de forma única su sitio web en Disqus.<br>Copia el <strong>nombre corto</strong> de tu sitio de <a class="bbc_link" href="https://disqus.com/admin/settings/general/" target="_blank" rel="noopener">desde la configuración de Disqus</a> y pégalo aquí.',
+];
