bf8647b57a56fc94dfc3f22bd3e68c5bf5049922
Bugo <bugo@dragomano.ru>
2022-01-06 23:27:24 +0500

    Add a new addon - Uicons

---
+++ Sources/LightPortal/Addons/Uicons/langs/english.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'description' => 'Support for <a class="bbc_link" href="https://www.flaticon.com/uicons" target="_blank" rel="noopener">Uicons</a> by Flaticon.',
+	'weight' => 'Icon Style (regular, bold, or solid)',
+	'corner' => 'Corners (round, or straight)',
+];
