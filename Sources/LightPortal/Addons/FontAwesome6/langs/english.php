1092cd3351617549bb84a1b302388eb436a06323
Bugo <bugo@dragomano.ru>
2022-01-09 14:15:09 +0500

    Add a new addon - FontAwesome6

---
+++ Sources/LightPortal/Addons/FontAwesome6/langs/english.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'description' => 'Support for <a class="bbc_link" href="https://fontawesome.com/v6.0" target="_blank" rel="noopener">Font Awesome 6</a> icons.',
+	'replace_ui_icons' => 'Replace current interface icons, if possible',
+];
