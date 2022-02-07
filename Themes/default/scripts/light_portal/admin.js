1fca46f308526269b883e64900c059e003f35f7b
Bugo <bugo@dragomano.ru>
2022-02-08 00:47:01 +0500

    Prevent possible errors on JS minification

--- Themes/default/scripts/light_portal/admin.js
+++ Themes/default/scripts/light_portal/admin.js
@@ -4,9 +4,9 @@ class PortalEntity {
 	}
 
 	async toggleStatus(target) {
-		const item = target.dataset.id
+		const item = target.dataset.id;
 
-		if (! item) return false
+		if (! item) return false;
 
 		let response = await fetch(this.workUrl, {
 			method: 'POST',
@@ -22,11 +22,11 @@ class PortalEntity {
 	}
 
 	async remove(target) {
-		if (! confirm(smf_you_sure)) return false
+		if (! confirm(smf_you_sure)) return false;
 
-		const item = target.dataset.id
+		const item = target.dataset.id;
 
-		if (! item) return false
+		if (! item) return false;
 
 		let response = await fetch(this.workUrl, {
 			method: 'POST',
@@ -42,18 +42,18 @@ class PortalEntity {
 	}
 
 	post(target) {
-		const formElements = target.elements
+		const formElements = target.elements;
 
 		for (let i = 0; i < formElements.length; i++) {
 			if ((formElements[i].required && formElements[i].value === '') || ! formElements[i].checkValidity()) {
-				let elem = formElements[i].closest('section').id
+				let elem = formElements[i].closest('section').id;
 
-				document.getElementsByName('tabs').checked = false
-				document.getElementById(elem.replace('content-', '')).checked = true
+				document.getElementsByName('tabs').checked = false;
+				document.getElementById(elem.replace('content-', '')).checked = true;
 
-				let focusElement = document.getElementById(formElements[i].id)
+				let focusElement = document.getElementById(formElements[i].id);
 
-				if (focusElement) focusElement.focus()
+				if (focusElement) focusElement.focus();
 
 				return false
 			}
@@ -68,9 +68,9 @@ class Block extends PortalEntity {
 	}
 
 	async clone(target) {
-		const item = target.dataset.id
+		const item = target.dataset.id;
 
-		if (! item) return false
+		if (! item) return false;
 
 		let response = await fetch(this.workUrl, {
 			method: 'POST',
@@ -82,38 +82,38 @@ class Block extends PortalEntity {
 			})
 		})
 
-		if (! response.ok) return console.error(response)
+		if (! response.ok) return console.error(response);
 
-		const json = await response.json()
+		const json = await response.json();
 
 		if (json.success) {
-			const div = target.cloneNode(true)
+			const div = target.cloneNode(true);
 
-			div.dataset.id = json.id
+			div.dataset.id = json.id;
 			target.after(div)
 		}
 	}
 
 	add(target) {
-		const thisForm = document.forms.block_add_form
+		const thisForm = document.forms.block_add_form;
 
-		thisForm.add_block.value = target.dataset.type
+		thisForm.add_block.value = target.dataset.type;
 		thisForm.submit()
 	}
 
 	async sort(e) {
-		const items = e.from.children
-		const items2 = e.to.children
+		const items = e.from.children;
+		const items2 = e.to.children;
 
-		let priority = []
-		let placement = ''
+		let priority = [];
+		let placement = '';
 
 		for (let i = 0; i < items2.length; i++) {
-			const key = items2[i].querySelector('.handle') ? parseInt(items2[i].querySelector('.handle').parentNode.parentNode.dataset.id, 10) : null
-			const place = items[i] && items[i].parentNode ? items[i].parentNode.dataset.placement : null
-			const place2 = items2[i] && items2[i].parentNode ? items2[i].parentNode.dataset.placement : null
+			const key = items2[i].querySelector('.handle') ? parseInt(items2[i].querySelector('.handle').parentNode.parentNode.dataset.id, 10) : null;
+			const place = items[i] && items[i].parentNode ? items[i].parentNode.dataset.placement : null;
+			const place2 = items2[i] && items2[i].parentNode ? items2[i].parentNode.dataset.placement : null;
 
-			if (place !== place2) placement = place2
+			if (place !== place2) placement = place2;
 
 			if (key !== null) priority.push(key)
 		}
@@ -129,10 +129,10 @@ class Block extends PortalEntity {
 			})
 		})
 
-		if (! response.ok) return console.error(response.status, priority)
+		if (! response.ok) return console.error(response.status, priority);
 
-		const nextElem = e.item.nextElementSibling
-		const prevElem = e.item.previousElementSibling
+		const nextElem = e.item.nextElementSibling;
+		const prevElem = e.item.previousElementSibling;
 
 		if (nextElem && nextElem.className === 'windowbg centertext') {
 			nextElem.remove()
@@ -149,9 +149,9 @@ class Page extends PortalEntity {
 	}
 
 	change(refs) {
-		pageType.disable()
+		pageType.disable();
 
-		if (refs.title_0.value) pageType.enable()
+		if (refs.title_0.value) pageType.enable();
 
 		// Create a page alias on page type changing
 		if (refs.alias.value === '' && typeof (slugify) === 'function') {
@@ -163,11 +163,11 @@ class Page extends PortalEntity {
 	}
 
 	toggleType(form) {
-		const pageContent = document.getElementById('content')
+		const pageContent = document.getElementById('content');
 
-		ajax_indicator(true)
+		ajax_indicator(true);
 
-		if (! pageContent.value) pageContent.value = ' '
+		if (! pageContent.value) pageContent.value = ' ';
 
 		form.preview.click()
 	}
@@ -180,8 +180,8 @@ class Plugin extends PortalEntity {
 	}
 
 	async toggle(target) {
-		const plugin = target.closest('.features').dataset.id
-		const status = target.dataset.toggle
+		const plugin = target.closest('.features').dataset.id;
+		const status = target.dataset.toggle;
 
 		let response = await fetch(this.workUrl + ';toggle', {
 			method: 'POST',
@@ -194,34 +194,34 @@ class Plugin extends PortalEntity {
 			})
 		})
 
-		if (! response.ok) return console.error(response)
+		if (! response.ok) return console.error(response);
 
-		let togglerClass
+		let togglerClass;
 
 		const classes = ['fa', 'bi']
 		classes.forEach(function(item) {
 			if ((new RegExp(item, 'i')).test(target.classList[1])) {
-				togglerClass = item
+				togglerClass = item;
 				return
 			}
 		});
 
 		if (target.dataset.toggle === 'on') {
-			target.classList.toggle(togglerClass + '-toggle-on')
-			target.classList.toggle(togglerClass + '-toggle-off')
+			target.classList.toggle(togglerClass + '-toggle-on');
+			target.classList.toggle(togglerClass + '-toggle-off');
 			target.setAttribute('data-toggle', 'off')
 		} else {
-			target.classList.toggle(togglerClass + '-toggle-off')
-			target.classList.toggle(togglerClass + '-toggle-on')
+			target.classList.toggle(togglerClass + '-toggle-off');
+			target.classList.toggle(togglerClass + '-toggle-on');
 			target.setAttribute('data-toggle', 'on')
 		}
 	}
 
 	showSettings(target) {
-		const el = document.getElementById(target.dataset.id + '_settings')
+		const el = document.getElementById(target.dataset.id + '_settings');
 
-		this.toggleSpin(target)
-		el.style.display = el.ownerDocument.defaultView.getComputedStyle(el, null).display === 'none' ? 'block' : 'none'
+		this.toggleSpin(target);
+		el.style.display = el.ownerDocument.defaultView.getComputedStyle(el, null).display === 'none' ? 'block' : 'none';
 		setTimeout(() => this.toggleSpin(target), 1000);
 	}
 
@@ -231,7 +231,7 @@ class Plugin extends PortalEntity {
 
 	async saveSettings(target, refs) {
 		let formData = new FormData(target),
-			lpCheckboxes = target.querySelectorAll('input[type=checkbox]')
+			lpCheckboxes = target.querySelectorAll('input[type=checkbox]');
 
 		lpCheckboxes.forEach(function (val) {
 			formData.append(val.getAttribute('name'), val.matches(':checked'))
@@ -242,35 +242,35 @@ class Plugin extends PortalEntity {
 			body: formData
 		})
 
-		this.fadeOut(refs.info)
+		this.fadeOut(refs.info);
 
 		return response.ok
 	}
 
 	fadeOut(el) {
-		let opacity = 1
+		let opacity = 1;
 		let	timer = setInterval(function () {
 			if (opacity <= 0.1) {
-				clearInterval(timer)
+				clearInterval(timer);
 				el.style.display = 'none'
 			}
 
-			el.style.opacity = opacity
+			el.style.opacity = opacity;
 			opacity -= opacity * 0.1
 		}, 400)
 	}
 
 	toggleToListView(el) {
-		document.getElementById('addon_list').classList.toggle('addon_list')
-		localStorage.setItem('lpAddonListView', 'list')
-		el.style.opacity = 1
+		document.getElementById('addon_list').classList.toggle('addon_list');
+		localStorage.setItem('lpAddonListView', 'list');
+		el.style.opacity = 1;
 		el.nextElementSibling.style.opacity = .5
 	}
 
 	toggleToCardView(el) {
-		document.getElementById('addon_list').classList.toggle('addon_list')
-		localStorage.setItem('lpAddonListView', 'card')
-		el.style.opacity = 1
+		document.getElementById('addon_list').classList.toggle('addon_list');
+		localStorage.setItem('lpAddonListView', 'card');
+		el.style.opacity = 1;
 		el.previousElementSibling.style.opacity = .5
 	}
 
@@ -286,12 +286,12 @@ class Category extends PortalEntity {
 	}
 
 	async updatePriority(e) {
-		const items = e.to.children
+		const items = e.to.children;
 
-		let priority = []
+		let priority = [];
 
 		for (let i = 0; i < items.length; i++) {
-			const id = items[i].querySelector('.handle') ? parseInt(items[i].querySelector('.handle').closest('tr').dataset.id, 10) : null
+			const id = items[i].querySelector('.handle') ? parseInt(items[i].querySelector('.handle').closest('tr').dataset.id, 10) : null;
 
 			if (id !== null) priority.push(id)
 		}
@@ -310,7 +310,7 @@ class Category extends PortalEntity {
 	}
 
 	async add(refs) {
-		if (! refs.cat_name) return false
+		if (! refs.cat_name) return false;
 
 		let response = await fetch(this.workUrl, {
 			method: 'POST',
@@ -323,22 +323,20 @@ class Category extends PortalEntity {
 			})
 		})
 
-		if (! response.ok) return console.error(response)
+		if (! response.ok) return console.error(response);
 
-		const json = await response.json()
+		const json = await response.json();
 
 		if (json.success) {
 			refs.category_list.insertAdjacentHTML('beforeend', json.section);
-
-			refs.cat_name.value = ''
-			refs.cat_desc.value = ''
-
+			refs.cat_name.value = '';
+			refs.cat_desc.value = '';
 			document.getElementById('category_desc' + json.item).focus()
 		}
 	}
 
 	async updateName(target, event) {
-		const item = target.dataset.id
+		const item = target.dataset.id;
 
 		if (item && event.value) {
 			let response = await fetch(this.workUrl, {
@@ -359,9 +357,9 @@ class Category extends PortalEntity {
 	}
 
 	async updateDescription(target, value) {
-		const item = target.dataset.id
+		const item = target.dataset.id;
 
-		if (! item) return false
+		if (! item) return false;
 
 		let response = await fetch(this.workUrl, {
 			method: 'POST',
