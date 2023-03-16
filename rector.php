9e7c595bf803abbbea1b77543cd2ca8814650d5c
Bugo <cheetah@bk.ru>
2023-03-16 06:03:25 +0500

    Install rector/rector as composer dependency

---
+++ rector.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php71\Rector\FuncCall\CountOnNullRector;
+use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/*',
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '/Sources/LightPortal/AddonHandler.php',
+        __DIR__ . '**/vendor/*',
+        __DIR__ . '**/langs/*',
+        __DIR__ . '/Sources/LightPortal/Addons/AvatarGenerator/Multiavatar.php',
+        CountOnNullRector::class,
+        JsonThrowOnErrorRector::class,
+    ]);
+
+    $rectorConfig->parallel(seconds: 360);
+
+    $rectorConfig->indent("\t", 4);
+
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses(false);
+
+    $rectorConfig->sets([
+        SetList::DEAD_CODE,
+        LevelSetList::UP_TO_PHP_80
+    ]);
+};
