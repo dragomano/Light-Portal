e4ef868dda7db699e8db03b8f0fa383558a58b81
Bugo <cheetah@bk.ru>
2023-03-25 22:37:23 +0500

    Add a new plugin - Crowdin Context

---
+++ Sources/LightPortal/Addons/CrowdinContext/langs/english.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+	'description' => 'This plugin allows you to enable remote translation of the Light Portal project via <a href="https://support.crowdin.com/ru/in-context-localization/" class="bbc_link">Crowdin In-Context</a>.',
+];
