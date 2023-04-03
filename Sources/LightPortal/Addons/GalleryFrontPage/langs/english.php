5cab3afcbf63ffeaf68e46eede15d9c03c98a846
Bugo <cheetah@bk.ru>
2023-04-03 16:34:29 +0500

    Update description of some plugins

--- Sources/LightPortal/Addons/GalleryFrontPage/langs/english.php
+++ Sources/LightPortal/Addons/GalleryFrontPage/langs/english.php
@@ -2,5 +2,7 @@
 
 return [
 	'description' => 'Displays SMF Gallery images on the frontpage.',
+	'caution' => 'the images will be displayed INSTEAD of portal articles on the frontpage!',
+	'note' => 'the plugin requires an installed modification of <a class="bbc_link" href="https://custom.simplemachines.org/index.php?mod=473">SMF Gallery</a> to work!',
 	'gallery_categories' => 'Gallery categories to display',
 ];
