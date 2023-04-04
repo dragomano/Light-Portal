84d389e6e5b031b279b69110fd4d9c5ce3c25567
Bugo <cheetah@bk.ru>
2023-04-04 17:17:57 +0500

    Update AdsBlock

--- Sources/LightPortal/Addons/AdsBlock/AdsBlock.php
+++ Sources/LightPortal/Addons/AdsBlock/AdsBlock.php
@@ -10,7 +10,7 @@
  * @license https://spdx.org/licenses/GPL-3.0-or-later.html GPL-3.0-or-later
  *
  * @category addon
- * @version 26.03.23
+ * @version 04.04.23
  */
 
 namespace Bugo\LightPortal\Addons\AdsBlock;
@@ -65,6 +65,8 @@ class AdsBlock extends Plugin
 		if ($this->context['lp_block']['type'] !== 'ads_block')
 			return;
 
+		$this->prepareTopicList();
+
 		$this->context['posting_fields']['loader_code']['label']['text'] = $this->txt['lp_ads_block']['loader_code'];
 		$this->context['posting_fields']['loader_code']['input'] = [
 			'type' => 'textarea',
@@ -103,29 +105,17 @@ class AdsBlock extends Plugin
 		$this->context['posting_fields']['ads_placement']['input']['html'] = '<div id="ads_placement" name="ads_placement"></div>';
 		$this->context['posting_fields']['ads_placement']['input']['tab']  = 'access_placement';
 
-		$this->context['posting_fields']['included_boards']['label']['text'] = $this->txt['lp_ads_block']['included_boards'];
-		$this->context['posting_fields']['included_boards']['input'] = [
-			'type' => 'text',
-			'after' => $this->txt['lp_ads_block']['included_boards_subtext'],
-			'attributes' => [
-				'maxlength' => 255,
-				'value'     => $this->context['lp_block']['options']['parameters']['included_boards'] ?? '',
-				'style'     => 'width: 100%'
-			],
-			'tab' => 'access_placement'
-		];
+		$this->context['posting_fields']['included_boards']['label']['html'] = '<label for="included_boards">' . $this->txt['lp_ads_block']['included_boards'] . '</label>';
+		$this->context['posting_fields']['included_boards']['input']['html'] = '<div id="included_boards" name="included_boards"></div>';
+		$this->context['posting_fields']['included_boards']['input']['tab']  = 'access_placement';
 
-		$this->context['posting_fields']['included_topics']['label']['text'] = $this->txt['lp_ads_block']['included_topics'];
-		$this->context['posting_fields']['included_topics']['input'] = [
-			'type' => 'text',
-			'after' => $this->txt['lp_ads_block']['included_topics_subtext'],
-			'attributes' => [
-				'maxlength' => 255,
-				'value'     => $this->context['lp_block']['options']['parameters']['included_topics'] ?? '',
-				'style'     => 'width: 100%'
-			],
-			'tab' => 'access_placement'
-		];
+		$this->context['lp_selected_boards'] = $this->getBoardList();
+
+		$this->context['posting_fields']['included_topics']['label']['html'] = '<label for="included_topics">' . $this->txt['lp_ads_block']['included_topics'] . '</label>';
+		$this->context['posting_fields']['included_topics']['input']['html'] = '<div id="included_topics" name="included_topics"></div>';
+		$this->context['posting_fields']['included_topics']['input']['tab']  = 'access_placement';
+
+		$this->context['lp_selected_topics'] = $this->getSelectedTopics();
 
 		$this->context['posting_fields']['end_date']['label']['html'] = '<label for="end_date">' . $this->txt['lp_ads_block']['end_date'] . '</label>';
 		$this->context['posting_fields']['end_date']['input']['html'] = '
@@ -136,9 +126,6 @@ class AdsBlock extends Plugin
 		$this->setTemplate()->withLayer('ads_block');
 	}
 
-	/**
-	 * @hook
-	 */
 	public function findBlockErrors(array $data, array &$post_errors)
 	{
 		if ($data['placement'] !== 'ads')
@@ -256,8 +243,7 @@ class AdsBlock extends Plugin
 		 *
 		 * Вывод рекламы перед первым сообщением
 		 */
-		if ($this->context['lp_ads_blocks']['before_first_post'] && $current_counter == $output['counter'] &&
-			empty($this->context['start'])) {
+		if ($this->context['lp_ads_blocks']['before_first_post'] && $current_counter == $output['counter'] && empty($this->context['start'])) {
 			lp_show_blocks('before_first_post');
 		}
 
@@ -305,8 +291,7 @@ class AdsBlock extends Plugin
 		 *
 		 * Вывод рекламы перед последним сообщением
 		 */
-		if ($this->context['lp_ads_blocks']['before_last_post'] &&
-			$output['id'] == ($showOldestFirst ? $this->context['topic_last_message'] : $this->context['topic_first_message'])) {
+		if ($this->context['lp_ads_blocks']['before_last_post'] && $output['id'] == ($showOldestFirst ? $this->context['topic_last_message'] : $this->context['topic_first_message'])) {
 			lp_show_blocks('before_last_post');
 		}
 
@@ -333,8 +318,7 @@ class AdsBlock extends Plugin
 		 *
 		 * Вывод рекламы после последнего сообщения
 		 */
-		if ($this->context['lp_ads_blocks']['after_last_post'] &&
-			$output['id'] == ($showOldestFirst ? $this->context['topic_last_message'] : $this->context['topic_first_message'])) {
+		if ($this->context['lp_ads_blocks']['after_last_post'] && $output['id'] == ($showOldestFirst ? $this->context['topic_last_message'] : $this->context['topic_first_message'])) {
 			ob_start();
 
 			lp_show_blocks('after_last_post');
@@ -442,5 +426,33 @@ class AdsBlock extends Plugin
 	{
 		return isset($this->context['lp_ads_block_plugin']['min_replies'])
 			&& $this->context['topicinfo']['num_replies'] < (int) $this->context['lp_ads_block_plugin']['min_replies'];
+	}
+
+	private function getSelectedTopics(): array
+	{
+		if (empty($this->context['lp_block']['options']['parameters']['included_topics']))
+			return [];
+
+		$request = $this->smcFunc['db_query']('', '
+			SELECT t.id_topic, m.subject
+			FROM {db_prefix}topics AS t
+				INNER JOIN {db_prefix}messages AS m ON (m.id_msg = t.id_first_msg)
+			WHERE t.id_topic IN ({array_int:topics})',
+			[
+				'topics' => explode(',', $this->context['lp_block']['options']['parameters']['included_topics']),
+			]
+		);
+
+		$topics = [];
+		while ($row = $this->smcFunc['db_fetch_assoc']($request)) {
+			$this->censorText($row['subject']);
+
+			$topics[$row['id_topic']] = $row['subject'];
+		}
+
+		$this->smcFunc['db_free_result']($request);
+		$this->context['lp_num_queries']++;
+
+		return $topics;
 	}
 }
