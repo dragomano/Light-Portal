780426d091d5f3850de995227ff5001aa3956968
Bugo <cheetah@bk.ru>
2023-04-09 14:58:28 +0500

    Remove GalleryFrontPage

--- Sources/LightPortal/Addons/GalleryFrontPage/langs/english.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-	'description' => 'Displays SMF Gallery images on the frontpage.',
-	'caution' => 'the images will be displayed INSTEAD of portal articles on the frontpage!',
-	'note' => 'the plugin requires an installed modification of <a class="bbc_link" href="https://custom.simplemachines.org/index.php?mod=473">SMF Gallery</a> to work!',
-	'gallery_categories' => 'Gallery categories to display',
-];
