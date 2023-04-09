60141af61b29444bf970fbf5ae58d6598df1f9e7
Bugo <cheetah@bk.ru>
2023-04-09 15:00:10 +0500

    Add a new plugin - GalleryBlock

---
+++ Sources/LightPortal/Addons/GalleryBlock/CategorySelect.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+
+/**
+ * CategorySelect.php
+ *
+ * @package GalleryBlock (Light Portal)
+ * @link https://custom.simplemachines.org/index.php?mod=4244
+ * @author Bugo <bugo@dragomano.ru>
+ * @copyright 2023 Bugo
+ * @license https://spdx.org/licenses/GPL-3.0-or-later.html GPL-3.0-or-later
+ *
+ * @category addon
+ * @version 09.04.23
+ */
+
+namespace Bugo\LightPortal\Addons\GalleryBlock;
+
+use Bugo\LightPortal\Partials\AbstractPartial;
+
+final class CategorySelect extends AbstractPartial
+{
+	public function __invoke(): string
+	{
+		$params['data'] = $this->getGalleryCategories();
+
+		$data = [];
+		foreach ($params['data'] as $id => $title) {
+			$data[] = [
+				'label' => $title,
+				'value' => $id
+			];
+		}
+
+		return /** @lang text */ '
+		<div id="categories" name="categories"></div>
+		<script>
+			VirtualSelect.init({
+				ele: "#categories",' . ($this->context['right_to_left'] ? '
+				textDirection: "rtl",' : '') . '
+				dropboxWrapper: "body",
+				multiple: true,
+				search: true,
+				markSearchResults: true,
+				placeholder: "' . $this->txt['lp_gallery_block']['categories_select'] . '",
+				noSearchResultsText: "' . $this->txt['no_matches'] . '",
+				searchPlaceholderText: "' . $this->txt['search'] . '",
+				allOptionsSelectedText: "' . $this->txt['all'] . '",
+				showValueAsTags: true,
+				maxWidth: "100%",
+				options: ' . json_encode($data) . ',
+				selectedValue: [' . ($this->context['lp_block']['options']['parameters']['categories'] ?? '') . ']
+			});
+		</script>';
+	}
+
+	private function getGalleryCategories(): array
+	{
+		$this->dbExtend();
+
+		if (empty($this->smcFunc['db_list_tables'](false, $this->db_prefix . 'gallery_cat')))
+			return [];
+
+		if (($categories = $this->cache()->get('smf_gallery_categories')) === null) {
+			$result = $this->smcFunc['db_query']('', '
+				SELECT id_cat, title
+				FROM {db_prefix}gallery_cat
+				WHERE redirect = {int:redirect}
+				ORDER BY roworder',
+				[
+					'redirect' => 0,
+				]
+			);
+
+			$categories = [];
+			while ($row = $this->smcFunc['db_fetch_assoc']($result))
+				$categories[$row['id_cat']] = $row['title'];
+
+			$this->smcFunc['db_free_result']($result);
+			$this->context['lp_num_queries']++;
+
+			$this->cache()->put('smf_gallery_categories', $categories);
+		}
+
+		return $categories;
+	}
+}
---
+++ Sources/LightPortal/Addons/GalleryBlock/langs/english.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'title' => 'Images from SMF Gallery',
+	'description' => 'Displays images from selected categories <a class="bbc_link" href="https://custom.simplemachines.org/index.php?mod=473">SMF Gallery</a>.',
+	'categories' => 'Image categories',
+	'categories_select' => 'If you do not need all the images, but only from certain categories',
+	'num_images' => 'Number of images to display',
+	'num_images_subtext' => 'Set to 0 to disable the limit',
+	'no_items' => 'There are no images in the selected categories.',
+];
