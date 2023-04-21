162208af507b416b2d7f08980e61e66746cebf69
Bugo <cheetah@bk.ru>
2023-04-21 07:12:08 +0500

    Add a new plugin - DiceBear

---
+++ Sources/LightPortal/Addons/DiceBear/langs/english.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'description' => 'With DiceBear you can provide your users with stunning random avatars in no time.',
+	'replace_missing_avatars' => 'Replace only missing avatars',
+	'source' => 'Data source for generation',
+	'source_set' => ['User name', 'User ID', 'User email'],
+	'style' => 'Style',
+	'size' => 'Default size',
+	'flip' => 'Flip right',
+];
