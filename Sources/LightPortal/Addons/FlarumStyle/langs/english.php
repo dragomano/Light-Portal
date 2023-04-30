731ebc2c3ed48db997bebb8bf2296d6c5bfb074c
Bugo <cheetah@bk.ru>
2023-05-01 00:47:05 +0500

    Remove FlarumStyle

--- Sources/LightPortal/Addons/FlarumStyle/langs/english.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	'description' => 'This addon will make a design of the frontpage articles like the style of the Flarum engine.',
-	'note' => 'It works if the frontpage displays forum topics or portal pages as articles!',
-	'disable_sidebar' => 'Disable the sidebar',
-	'all_boards' => 'All boards',
-	'tags' => 'Tags'
-];
