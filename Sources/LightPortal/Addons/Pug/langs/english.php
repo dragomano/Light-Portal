e13ad7f782e372e27eab2fe5e5a41e96105c7962
Bugo <cheetah@bk.ru>
2023-05-09 09:14:06 +0500

    Remove Pug and Twig plugins

--- Sources/LightPortal/Addons/Pug/langs/english.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-	'title' => 'Custom Pug',
-	'description' => 'Adds the ability to use the Pug preprocessor to design the content of portal blocks and pages.',
-	'block_desc' => 'In this block, you can use the <a href="https://pugjs.org/language/attributes.html" target="_blank" rel="noopener">Pug</a> syntax as content.',
-];
