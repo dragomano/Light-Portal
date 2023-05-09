2291e646ec98b651681596755dbf63e8a636d2b6
Bugo <cheetah@bk.ru>
2023-05-09 08:07:16 +0500

    Add a new plugin - Reactions

---
+++ Sources/LightPortal/Addons/Reactions/langs/english.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'description' => 'Adds reactions for pages and comments.',
+	'allow_reactions' => 'Allow reactions',
+	'titles' => ['Like', 'Dislike', 'Love', 'Laugh', 'Sad', 'Angry'],
+];
