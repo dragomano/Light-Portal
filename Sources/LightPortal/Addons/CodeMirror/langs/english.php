f8a5d5f0251336be332b16f0227fc24754560b21
Bugo <cheetah@bk.ru>
2023-05-15 00:19:35 +0500

    Update some plugins

--- Sources/LightPortal/Addons/CodeMirror/langs/english.php
+++ Sources/LightPortal/Addons/CodeMirror/langs/english.php
@@ -2,6 +2,6 @@
 
 return [
 	'description' => 'Provides syntax highlighting when editing pages and blocks with various content.',
-	'mode' => 'Syntax Highlighting',
+	'modes' => 'Syntax Highlighting',
 	'small_hint' => 'Disable other editor plugins so that content blocks are not duplicated.',
 ];
