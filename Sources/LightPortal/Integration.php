7f8457a59ced1eb9ed9f01fba5cf7e100cb90baf
Bugo <cheetah@bk.ru>
2023-05-29 09:13:53 +0500

    Add a new constant - LP_ADDON_URL

--- Sources/LightPortal/Integration.php
+++ Sources/LightPortal/Integration.php
@@ -59,6 +59,7 @@ final class Integration extends AbstractMain
 
 		defined('LP_NAME') || define('LP_NAME', 'Light Portal');
 		defined('LP_VERSION') || define('LP_VERSION', '2.2.0 Mandalorian Edition');
+		defined('LP_ADDON_URL') || define('LP_ADDON_URL', $this->boardurl . '/Sources/LightPortal/Addons');
 		defined('LP_ADDON_DIR') || define('LP_ADDON_DIR', __DIR__ . '/Addons');
 		defined('LP_CACHE_TIME') || define('LP_CACHE_TIME', (int) ($this->modSettings['lp_cache_update_interval'] ?? 72000));
 		defined('LP_ACTION') || define('LP_ACTION', $this->modSettings['lp_portal_action'] ?? 'portal');
