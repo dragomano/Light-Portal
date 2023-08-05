cddfc7a1c826c998bd57a7461e19f0b50344043a
Bugo <cheetah@bk.ru>
2023-08-05 13:33:07 +0500

    Update rector

--- rector.php
+++ rector.php
@@ -1,14 +1,17 @@
 <?php declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php71\Rector\FuncCall\CountOnNullRector;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
+
     $rectorConfig->paths([
-        __DIR__ . '/src/*',
+        __DIR__ . '/src',
     ]);
 
     $rectorConfig->skip([
