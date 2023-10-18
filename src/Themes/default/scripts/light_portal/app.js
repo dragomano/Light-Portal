5ab0ab41774d22e1d4debc93ebababc81cb58702
Bugo <cheetah@bk.ru>
2023-10-18 21:34:28 +0500

    Add Alpine to globals

--- src/Themes/default/scripts/light_portal/app.js
+++ src/Themes/default/scripts/light_portal/app.js
@@ -3,8 +3,8 @@ import slug from 'alpinejs-slug'
 import styleNames from '@shat/stylenames'
 import axios from 'axios'
 
+window.Alpine = Alpine
 window.styleNames = styleNames
-
 window.axios = axios
 window.axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8'
 
