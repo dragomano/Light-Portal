9c48976da523780ae6eec1e4a365627411535507
Bugo <cheetah@bk.ru>
2023-10-29 18:27:56 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-const dist = resolve('./src/Themes/default/scripts/light_portal')
+const dist = resolve('./src/Themes/default/scripts/light_portal');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
       output: {
         dir: dist,
         entryFileNames: 'bundle.min.js',
-        format: 'iife'
+        format: 'iife',
       },
     },
   },
@@ -25,7 +25,7 @@ export default defineConfig({
         { src: 'node_modules/vanilla-lazyload/dist/lazyload.esm.min.js', dest: dist },
         { src: 'node_modules/virtual-select-plugin/dist/virtual-select.min.css', dest: dist },
         { src: 'node_modules/virtual-select-plugin/dist/virtual-select.min.js', dest: dist },
-      ]
-    })
-  ]
-})
+      ],
+    }),
+  ],
+});
