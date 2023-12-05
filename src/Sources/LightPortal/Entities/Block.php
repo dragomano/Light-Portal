55fbb539216d1b6bd462dbfef1b86534c8e04d13
Bugo <cheetah@bk.ru>
2023-12-06 00:11:51 +0500

    Fix the issue with preview mode for blocks

--- src/Sources/LightPortal/Entities/Block.php
+++ src/Sources/LightPortal/Entities/Block.php
@@ -25,7 +25,7 @@ final class Block
 
 	public function show(): void
 	{
-		if ($this->isHideBlocksInAdmin() || $this->request()->is('devtools'))
+		if ($this->isHideBlocksInAdmin() || $this->request()->is('devtools') || $this->request()->has('preview'))
 			return;
 
 		if (empty($this->context['allow_light_portal_view']) || empty($this->context['template_layers']) || empty($this->context['lp_active_blocks']))
