efba06b07faa0a48c7a9a585a4271f8c2a309dee
Bugo <cheetah@bk.ru>
2023-12-17 02:40:09 +0500

    Add a new plugin - Snowflakes

---
+++ src/Sources/LightPortal/Addons/Snowflakes/langs/english.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'description' => 'Adds a New Year mood to the site, more specifically snowfall, snowdrifts and a car that clears them regularly!',
+	'icon_color' => 'Snowflake color',
+	'icon_size' => 'Snowflake size',
+	'snowflakes_count' => 'Number of snowflakes',
+	'enable_snowdrifts' => 'Enable snowdrifts',
+	'snowdrifts_count' => 'Number of snowdrifts',
+];
