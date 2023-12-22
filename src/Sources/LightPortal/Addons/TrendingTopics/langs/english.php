a9c66e3060d031d25fc24929ed22b15bc6543315
Bugo <cheetah@bk.ru>
2023-12-23 03:10:32 +0500

    Add a new plugin - TrendingTopics

---
+++ src/Sources/LightPortal/Addons/TrendingTopics/langs/english.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'title' => 'Trending Topics',
+	'description' => 'Displays the most trending forum topics for a specified period of time.',
+	'show_avatars' => 'Show user avatars',
+	'time_period' => 'Time period (to determine the frequency of replies)',
+	'time_period_set' => ['Day', 'Week', '2 weeks', 'Month', '2 months', '4 months', '6 months', '8 months', 'Year'],
+	'min_replies' => 'Minimum number of replies for the specified period',
+	'num_topics' => 'Number of topics to display',
+	'none' => 'There are no trending topics',
+];
