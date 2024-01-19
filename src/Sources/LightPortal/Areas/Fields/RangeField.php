77e2974b8a5c1be20223942fb144360c2a7c525d
Bugo <cheetah@bk.ru>
2024-01-20 00:08:22 +0500

    Update version

--- src/Sources/LightPortal/Areas/Fields/RangeField.php
+++ src/Sources/LightPortal/Areas/Fields/RangeField.php
@@ -9,7 +9,7 @@
  * @copyright 2019-2024 Bugo
  * @license https://spdx.org/licenses/GPL-3.0-or-later.html GPL-3.0-or-later
  *
- * @version 2.4
+ * @version 2.5
  */
 
 namespace Bugo\LightPortal\Areas\Fields;
