36b84d6958e19b6118cd39c8e8edbb2d514f4b03
Bugo <cheetah@bk.ru>
2024-01-20 00:04:27 +0500

    Update install and uninstall scripts

--- src/database.php
+++ src/database.php
@@ -2,16 +2,16 @@
 
 global $user_info, $mbname, $modSettings, $settings, $smcFunc;
 
-if (file_exists(__DIR__ . '/SSI.php') && ! defined('SMF'))
-	require_once __DIR__ . '/SSI.php';
-elseif(! defined('SMF'))
-	die('<b>Error:</b> Cannot install - please verify that you put this file in the same place as SMF\'s index.php and SSI.php files.');
-
 if (version_compare(PHP_VERSION, '8.0', '<'))
 	die('This mod needs PHP 8.0 or greater. You will not be able to install/use this mod. Please, contact your host and ask for a php upgrade.');
 
 if (! extension_loaded('intl'))
 	die('This mod needs intl extension to properly work with plurals, locale-aware numbers, and much more. Contact your host or install this extension by manual.');
+
+if (file_exists(__DIR__ . '/SSI.php') && ! defined('SMF'))
+	require_once __DIR__ . '/SSI.php';
+elseif(! defined('SMF'))
+	die('<b>Error:</b> Cannot install - please verify that you put this file in the same place as SMF\'s index.php and SSI.php files.');
 
 if ((SMF === 'SSI') && ! $user_info['is_admin'])
 	die('Admin privileges required.');
