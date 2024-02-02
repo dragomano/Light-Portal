67bbf5a499595a89a720e9c203dbbbad22c1eb02
Bugo <cheetah@bk.ru>
2024-02-03 00:48:14 +0500

    Refactor app.php

--- src/Sources/LightPortal/app.php
+++ src/Sources/LightPortal/app.php
@@ -8,7 +8,7 @@ if (! defined('SMF'))
 require_once __DIR__ . '/Libs/autoload.php';
 
 use Laminas\Loader\StandardAutoloader;
-use Bugo\LightPortal\{AddonHandler, Integration};
+use Bugo\LightPortal\Integration;
 use Bugo\LightPortal\Utils\{Config, Lang, Theme, User, Utils};
 
 // Register autoloader
@@ -17,22 +17,28 @@ $loader->registerNamespace('Bugo\LightPortal', __DIR__);
 $loader->register();
 
 if (str_starts_with(SMF_VERSION, '3.0')) {
-	class_alias('SMF\\ServerSideIncludes', 'Bugo\\LightPortal\\Utils\\ServerSideIncludes');
-	class_alias('SMF\\IntegrationHook', 'Bugo\\LightPortal\\Utils\\IntegrationHook');
-	class_alias('SMF\\ErrorHandler', 'Bugo\\LightPortal\\Utils\\ErrorHandler');
-	class_alias('SMF\\BBCodeParser', 'Bugo\\LightPortal\\Utils\\BBCodeParser');
-	class_alias('SMF\\Cache\\CacheApi', 'Bugo\\LightPortal\\Utils\\CacheApi');
-	class_alias('SMF\\Actions\\Notify', 'Bugo\\LightPortal\\Utils\\Notify');
-	class_alias('SMF\\Theme', 'Bugo\\LightPortal\\Utils\\SMFTheme');
-	class_alias('SMF\\Config', 'Bugo\\LightPortal\\Utils\\Config');
-	class_alias('SMF\\Lang', 'Bugo\\LightPortal\\Utils\\SMFLang');
-	class_alias('SMF\\Utils', 'Bugo\\LightPortal\\Utils\\Utils');
-	class_alias('SMF\\Mail', 'Bugo\\LightPortal\\Utils\\Mail');
-	class_alias('SMF\\User', 'Bugo\\LightPortal\\Utils\\User');
-	class_alias('SMF\\Sapi', 'Bugo\\LightPortal\\Utils\\Sapi');
-	class_alias('Bugo\\LightPortal\\Actions\\BoardIndexNext', 'Bugo\\LightPortal\\Actions\\BoardIndex');
-	class_alias('Bugo\\LightPortal\\Utils\\SMFTraitNext', 'Bugo\\LightPortal\\Utils\\SMFTrait');
-	class_alias('Bugo\\LightPortal\\Utils\\LangNext', 'Bugo\\LightPortal\\Utils\\Lang');
+	$aliases = [
+		'SMF\\ServerSideIncludes'                    => 'Bugo\\LightPortal\\Utils\\ServerSideIncludes',
+		'SMF\\IntegrationHook'                       => 'Bugo\\LightPortal\\Utils\\IntegrationHook',
+		'SMF\\ErrorHandler'                          => 'Bugo\\LightPortal\\Utils\\ErrorHandler',
+		'SMF\\BBCodeParser'                          => 'Bugo\\LightPortal\\Utils\\BBCodeParser',
+		'SMF\\Cache\\CacheApi'                       => 'Bugo\\LightPortal\\Utils\\CacheApi',
+		'SMF\\Actions\\Notify'                       => 'Bugo\\LightPortal\\Utils\\Notify',
+		'SMF\\Theme'                                 => 'Bugo\\LightPortal\\Utils\\SMFTheme',
+		'SMF\\Config'                                => 'Bugo\\LightPortal\\Utils\\Config',
+		'SMF\\Lang'                                  => 'Bugo\\LightPortal\\Utils\\SMFLang',
+		'SMF\\Utils'                                 => 'Bugo\\LightPortal\\Utils\\Utils',
+		'SMF\\Mail'                                  => 'Bugo\\LightPortal\\Utils\\Mail',
+		'SMF\\User'                                  => 'Bugo\\LightPortal\\Utils\\User',
+		'SMF\\Sapi'                                  => 'Bugo\\LightPortal\\Utils\\Sapi',
+		'Bugo\\LightPortal\\Actions\\BoardIndexNext' => 'Bugo\\LightPortal\\Actions\\BoardIndex',
+		'Bugo\\LightPortal\\Utils\\SMFTraitNext'     => 'Bugo\\LightPortal\\Utils\\SMFTrait',
+		'Bugo\\LightPortal\\Utils\\LangNext'         => 'Bugo\\LightPortal\\Utils\\Lang',
+	];
+
+	$applyAlias = fn($class, $alias) => class_alias($class, $alias);
+
+	array_map($applyAlias, array_keys($aliases), $aliases);
 } else {
 	array_map(fn($u) => new $u(), [
 		Lang::class,
