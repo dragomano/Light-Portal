a700ecfac8c35ed6937261daf1ea2bff7c0cde6d
Bugo <cheetah@bk.ru>
2024-02-05 00:19:46 +0500

    Update ViewDebug template

--- src/Themes/default/LightPortal/ViewDebug.template.php
+++ src/Themes/default/LightPortal/ViewDebug.template.php
@@ -7,7 +7,11 @@ function template_docs_above(): void
 	$lang = in_array(Lang::$txt['lang_dictionary'], ['ru', 'el', 'it']) ? Lang::$txt['lang_dictionary'] : '';
 
 	echo '
-	<div class="noticebox"><a class="bbc_link" href="https://dragomano.github.io/Light-Portal/' . $lang . '" target="_blank" rel="noopener">', Lang::$txt['admin_search_type_online'], '</a></div>';
+	<div class="noticebox">
+		<a class="bbc_link" href="https://dragomano.github.io/Light-Portal/' . $lang . '" target="_blank" rel="noopener">
+			', Lang::$txt['admin_search_type_online'], '
+		</a>
+	</div>';
 }
 
 function template_docs_below()
@@ -21,5 +25,7 @@ function template_debug_above()
 function template_debug_below(): void
 {
 	echo '
-	<div class="centertext clear noticebox smalltext" style="margin-top: 2px">', Utils::$context['lp_load_page_stats'], '</div>';
+	<div class="centertext clear noticebox smalltext" style="margin-top: 2px">
+		', Utils::$context['lp_load_page_stats'], '
+	</div>';
 }
