a5b555e6bdade4b12ef631fbb712867dd64d4623
Bugo <cheetah@bk.ru>
2024-02-09 05:27:00 +0500

    Update dependencies

--- rector.php
+++ rector.php
@@ -1,33 +1,19 @@
 <?php declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\ValueObject\PhpVersion;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
-use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(PhpVersion::PHP_80);
-
-    $rectorConfig->paths([
-        __DIR__ . '/src',
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__ . '/src/database.php',
-        __DIR__ . '**/Libs/*',
-        __DIR__ . '**/vendor/*',
-        __DIR__ . '**/langs/*',
-        JsonThrowOnErrorRector::class,
-    ]);
-
-    $rectorConfig->parallel(360);
-    $rectorConfig->indent("\t", 4);
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-
-    $rectorConfig->sets([
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_80
-    ]);
-};
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/src',
+	])
+	->withSkip([
+		__DIR__ . '/src/database.php',
+		__DIR__ . '**/Libs/*',
+		__DIR__ . '**/vendor/*',
+		__DIR__ . '**/langs/*',
+	])
+	->withParallel(360)
+	->withIndent(indentChar: "\t")
+	->withImportNames(importShortClasses: false, removeUnusedImports: true)
+	->withPreparedSets(deadCode: true)
+	->withPhpSets();
