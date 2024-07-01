85186e2d7de68fb0b6143caada7dae114f3ff522
Bugo <cheetah@bk.ru>
2024-07-02 00:09:12 +0500

    Add fixes proposed by Rector

--- src/Sources/LightPortal/Utils/EntityManager.php
+++ src/Sources/LightPortal/Utils/EntityManager.php
@@ -12,7 +12,12 @@
 
 namespace Bugo\LightPortal\Utils;
 
-use Bugo\LightPortal\Lists;
+use Bugo\LightPortal\Lists\CategoryList;
+use Bugo\LightPortal\Lists\IconList;
+use Bugo\LightPortal\Lists\PageList;
+use Bugo\LightPortal\Lists\PluginList;
+use Bugo\LightPortal\Lists\TagList;
+use Bugo\LightPortal\Lists\TitleList;
 
 if (! defined('SMF'))
 	die('No direct access...');
@@ -24,12 +29,12 @@ final class EntityManager
 	public function __invoke(string $entity): array
 	{
 		return match ($entity) {
-			'category' => $this->cache('all_categories')->setFallback(Lists\CategoryList::class),
-			'page'     => $this->cache('all_pages')->setFallback(Lists\PageList::class),
-			'tag'      => $this->cache('all_tags')->setFallback(Lists\TagList::class),
-			'title'    => $this->cache('all_titles')->setFallback(Lists\TitleList::class),
-			'icon'     => $this->cache('all_icons')->setFallback(Lists\IconList::class),
-			'plugin'   => (new Lists\PluginList())(),
+			'category' => $this->cache('all_categories')->setFallback(CategoryList::class),
+			'page'     => $this->cache('all_pages')->setFallback(PageList::class),
+			'tag'      => $this->cache('all_tags')->setFallback(TagList::class),
+			'title'    => $this->cache('all_titles')->setFallback(TitleList::class),
+			'icon'     => $this->cache('all_icons')->setFallback(IconList::class),
+			'plugin'   => (new PluginList())(),
 			default    => [],
 		};
 	}
