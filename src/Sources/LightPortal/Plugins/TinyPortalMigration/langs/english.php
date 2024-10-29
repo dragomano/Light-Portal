42cc9885eb21a238a2bc51cd2d35bfc54fe8ecb8
Bugo <cheetah@bk.ru>
2024-10-29 19:27:20 +0500

    Update TinyPortalMigration

--- src/Sources/LightPortal/Plugins/TinyPortalMigration/langs/english.php
+++ src/Sources/LightPortal/Plugins/TinyPortalMigration/langs/english.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'description' => 'Support for importing TinyPortal pages, categories, c and custom blocks (if they exist in the database).',
+	'description' => 'Support for importing TinyPortal pages, categories, and custom blocks (if they exist in the database).',
 	'label_name' => 'Import from TinyPortal',
 	'block_import_desc' => 'Import custom TinyPortal blocks (BBCode, HTML, PHP).',
 	'page_import_desc' => 'Import TinyPortal pages (if they exist in the database).',
