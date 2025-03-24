9eb591793dcbd6be5b5ffae51fe02e88da1bca2b
Bugo <cheetah@bk.ru>
2025-03-24 20:05:34 +0500

    Use Robo task runner instead of phing

---
+++ RoboFile.php
@@ -0,0 +1,187 @@
+<?php declare(strict_types=1);
+
+use Robo\Symfony\ConsoleIO;
+use Robo\Tasks;
+
+class RoboFile extends Tasks
+{
+	private array $baseExclusions = [
+		'.idea',
+		'src/Sources/index.php',
+		'src/Themes/default/css/light_portal/plugins.css',
+		'src/Themes/default/scripts/light_portal/plugins.js',
+	];
+
+	private array $premiumPlugins = [
+		'ApexCharts',
+		'DiceBear',
+		'ImageUpload',
+		'IndexNow',
+		'Jodit',
+		'MediaBlock',
+		'PageScroll',
+	];
+
+	private array $remotePlugins = [
+		'AdsBlock',
+		'BootstrapIcons',
+		'Disqus',
+		'EasyMarkdownEditor',
+		'EhPortalMigration',
+		'EzPortalMigration',
+		'FaBoardIcons',
+		'FacebookComments',
+		'Giscus',
+		'LatteLayouts',
+		'LineAwesomeIcons',
+		'Markdown',
+		'MaterialDesignIcons',
+		'Optimus',
+		'PluginMaker',
+		'PrettyUrls',
+		'SiteList',
+		'Snowflakes',
+		'TagList',
+		'TinyPortalMigration',
+		'TopicRatingBar',
+		'TwigLayouts',
+		'Uicons',
+		'VkComments',
+	];
+
+	private array $partialTranslations = [
+		'arabic',
+		'czech',
+		'danish',
+		'dutch',
+		'french',
+		'german',
+		'norvegian',
+		'portuguese_pt',
+		'spanish_es',
+		'spanish_latin',
+		'swedish',
+		'ukrainian',
+	];
+
+	private string $version;
+
+	private array $packageTypes = [
+		'default',
+		'geek',
+		'develop',
+	];
+
+	public function __construct()
+	{
+		$this->version = $this->getVersionFromXml();
+	}
+
+	public function build(ConsoleIO $io, $opts = ['type' => false]): void
+	{
+		$type = $opts['type'];
+
+		if (! $type) {
+			$type = $io->choice('What is the package type?', $this->packageTypes, 0);
+		}
+
+		$yes = $io->confirm('Do you want to change version?');
+
+		if ($yes) {
+			$this->version = $io->ask('What is the package version?');
+
+			$this->changeVersion($this->version);
+		}
+
+		switch ($type) {
+			case 'geek':
+				$this->buildGeek();
+				break;
+			case 'develop':
+				$this->buildDevelop();
+				break;
+			default:
+				$this->buildDefault();
+		}
+
+		$io->say('Done!');
+	}
+
+	public function buildDefault(): void
+	{
+		$file = "light_portal_$this->version.tgz";
+
+		$this->_remove($file);
+
+		$this->taskPack($file)
+			->addDir('.', 'src')
+			->addFile('./LICENSE', 'LICENSE')
+			->exclude('.meta-storm.xml')
+			->exclude([...$this->baseExclusions, ...$this->premiumPlugins, ...$this->remotePlugins])
+			->exclude(['*(' . implode('|', $this->partialTranslations) . ').php'])
+			->run();
+	}
+
+	public function buildGeek(): void
+	{
+		$file = "light_portal_{$this->version}_geek_edition.tgz";
+
+		$this->_remove($file);
+
+		$this->taskPack($file)
+			->addDir('.', 'src')
+			->addFile('./LICENSE', 'LICENSE')
+			->exclude('.meta-storm.xml')
+			->exclude([...$this->baseExclusions, ...$this->premiumPlugins])
+			->exclude($this->getChildren())
+			->exclude(['languages/LightPortal/(?!index|LightPortal\.english).*\.php'])
+			->run();
+	}
+
+	public function buildDevelop(): void
+	{
+		$file = "light_portal_{$this->version}_dev_edition.tgz";
+
+		$this->_remove($file);
+
+		$this->taskPack($file)
+			->addDir('.', 'src')
+			->addFile('./LICENSE', 'LICENSE')
+			->exclude([...$this->baseExclusions, ...$this->premiumPlugins])
+			->exclude(['langs/(?!index|english).*\.php'])
+			->exclude(['languages/LightPortal/(?!index|LightPortal\.english).*\.php'])
+			->run();
+	}
+
+	private function getVersionFromXml(): string
+	{
+		$xml = simplexml_load_file('src/package-info.xml');
+
+		return (string) $xml->version;
+	}
+
+	private function changeVersion(string $version): void
+	{
+		$this->taskReplaceInFile('src/package-info.xml')
+			->regex('~(<version>)(.*?)(<\/version>)~')
+			->to("<version>$version</version>")
+			->run();
+
+		$this->taskReplaceInFile('src/Sources/LightPortal/Hooks/Init.php')
+			->regex("~'LP_VERSION',\s*'(\d+\.\d+(?:.\d+)?)'~")
+			->to("'LP_VERSION', '$version'")
+			->run();
+	}
+
+	private function getChildren(): array
+	{
+		$directories = glob(__DIR__ . '/src/Sources/LightPortal/Plugins/*', GLOB_ONLYDIR);
+
+		$names = [];
+		foreach ($directories as $path) {
+			$names[] = 'Plugins/' . basename($path);
+		}
+
+		return $names;
+	}
+}
--- src/Sources/LightPortal/Areas/CreditArea.php
+++ src/Sources/LightPortal/Areas/CreditArea.php
@@ -159,6 +159,10 @@ final class CreditArea
 				'name' => 'PHPStorm',
 				'link' => 'https://www.jetbrains.com/phpstorm/'
 			],
+			[
+				'name' => 'Robo',
+				'link' => 'http://robo.li/'
+			],
 		];
 
 		$links = [
