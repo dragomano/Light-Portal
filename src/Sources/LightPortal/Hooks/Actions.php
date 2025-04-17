93d24cd5f078b81d510447be37eed0f36fdb55a9
Bugo <cheetah@bk.ru>
2025-04-17 11:05:47 +0500

    Fix issue with portal subactions when frontpage mode was disabled

--- src/Sources/LightPortal/Hooks/Actions.php
+++ src/Sources/LightPortal/Hooks/Actions.php
@@ -13,10 +13,18 @@
 namespace Bugo\LightPortal\Hooks;
 
 use Bugo\Compat\Config;
+use Bugo\Compat\User;
 use Bugo\LightPortal\Actions\BoardIndex;
+use Bugo\LightPortal\Actions\Category;
 use Bugo\LightPortal\Actions\FrontPage;
+use Bugo\LightPortal\Actions\Tag;
 use Bugo\LightPortal\Enums\Action;
+use Bugo\LightPortal\Enums\PortalSubAction;
 use Bugo\LightPortal\Utils\Setting;
+use Bugo\LightPortal\Utils\Traits\HasCache;
+
+use function array_search;
+use function implode;
 
 use const LP_ACTION;
 
@@ -33,6 +41,8 @@ class Actions
 			$actions[LP_ACTION] = [false, [app(FrontPage::class), 'show']];
 		}
 
+		$this->handleSubActions();
+
 		$actions[Action::FORUM->value] = [false, [app(BoardIndex::class), 'show']];
 
 		if (empty(Config::$modSettings['lp_standalone_mode']))
@@ -40,5 +50,43 @@ class Actions
 
 		$this->unsetDisabledActions($actions);
 		$this->redirectFromDisabledActions();
+	}
+
+	private function handleSubActions(): void
+	{
+		$sa = $this->request()->get('sa') ?? '';
+
+		if ($this->request()->is(LP_ACTION)) {
+			match ($sa) {
+				PortalSubAction::CATEGORIES->name() => app(Category::class)->show(),
+				PortalSubAction::TAGS->name()       => app(Tag::class)->show(),
+				PortalSubAction::PROMOTE->name()    => $this->promoteTopic(),
+				default                             => null,
+			};
+		}
+	}
+
+	private function promoteTopic(): void
+	{
+		if (empty(User::$me->is_admin) || $this->request()->hasNot('t'))
+			return;
+
+		$topic = $this->request()->get('t');
+
+		$homeTopics = Setting::getFrontpageTopics();
+
+		if (($key = array_search($topic, $homeTopics)) !== false) {
+			unset($homeTopics[$key]);
+		} else {
+			$homeTopics[] = $topic;
+		}
+
+		Config::updateModSettings(
+			['lp_frontpage_topics' => implode(',', $homeTopics)]
+		);
+
+		$this->cache()->flush();
+
+		$this->response()->redirect('topic=' . $topic);
 	}
 }
