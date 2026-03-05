02508d435184aeedd36c930c3dbceac51749993f
Bugo <229402+dragomano@users.noreply.github.com>
2026-03-06 04:24:17 +0500

    Use CSS Grid instead tables (CurrentMonth)

--- src/Sources/LightPortal/Plugins/CurrentMonth/views/default.blade.php
+++ src/Sources/LightPortal/Plugins/CurrentMonth/views/default.blade.php
@@ -1,53 +1,49 @@
-<table
-	class="table_grid"
+<div
+	class="calendar_grid"
 	role="grid"
 	aria-label="{{ ($txt['months_titles'][$data['current_month']] ?? '') . ' ' . ($data['current_year'] ?? '') }}"
 >
 	@if (empty($data['disable_day_titles']))
-		<thead>
-			<tr class="title_bar">
-				@foreach ($data['week_days'] as $day)
-					<th scope="col">
-						{{ $txt['days_short'][$day] ?? $day }}
-					</th>
-				@endforeach
-			</tr>
-		</thead>
+		<div class="calendar_grid__header" role="row">
+			@foreach ($data['week_days'] as $day)
+				<div class="calendar_grid__weekday" role="columnheader">
+					{{ $txt['days_short'][$day] ?? $day }}
+				</div>
+			@endforeach
+		</div>
 	@endif
 
-	@foreach ($data['weeks'] as $week)
-		<tbody>
-			<tr class="days_wrapper">
-				@foreach ($week['days'] as $day)
-					@php
-						$classes = ['days'];
+	<div class="calendar_grid__body">
+		@foreach ($data['weeks'] as $week)
+			@foreach ($week['days'] as $day)
+				@php
+					$classes = ['calendar_grid__day'];
 
-						if (empty($day['day'])) {
-							$classes[] = 'disabled';
-						} else {
-							$classes[] = empty($day['is_today']) ? 'windowbg' : 'calendar_today';
+					if (empty($day['day'])) {
+						$classes[] = 'disabled';
+					} else {
+						$classes[] = empty($day['is_today']) ? 'windowbg' : 'calendar_today';
 
-							foreach (['events', 'holidays', 'birthdays'] as $type) {
-								if (! empty($day[$type])) {
-									$classes[] = $type;
-								}
+						foreach (['events', 'holidays', 'birthdays'] as $type) {
+							if (! empty($day[$type])) {
+								$classes[] = $type;
 							}
 						}
-					@endphp
+					}
+				@endphp
 
-					<td class="{{ implode(' ', $classes) }}">
-						@unless (empty($day['day']))
-							@if (empty($modSettings['cal_enabled']))
+				<div class="{{ implode(' ', $classes) }}" role="gridcell">
+					@unless (empty($day['day']))
+						@if (empty($modSettings['cal_enabled']))
+							<span class="day_text">{{ $day['day'] }}</span>
+						@else
+							<a href="{{ $scripturl }}?action=calendar;viewlist;year={{ $data['current_year'] }};month={{ $data['current_month'] }};day={{ $day['day'] }}">
 								<span class="day_text">{{ $day['day'] }}</span>
-							@else
-								<a href="{{ $scripturl }}?action=calendar;viewlist;year={{ $data['current_year'] }};month={{ $data['current_month'] }};day={{ $day['day'] }}">
-									<span class="day_text">{{ $day['day'] }}</span>
-								</a>
-							@endif
-						@endunless
-					</td>
-				@endforeach
-			</tr>
-		</tbody>
-	@endforeach
-</table>
+							</a>
+						@endif
+					@endunless
+				</div>
+			@endforeach
+		@endforeach
+	</div>
+</div>
